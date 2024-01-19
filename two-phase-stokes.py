19c204e7cb33e81cef4dcd75c779b0c34bc0d695
zhixuan-zxli <zihinlai@163.com>
2024-01-19 13:26:11 +0800

    Implement P1+P0 pressure space manually. Looks OK.

--- two-phase-stokes.py
+++ two-phase-stokes.py
@@ -5,7 +5,6 @@ from matplotlib import pyplot
 
 # load the mesh
 mesh, boundary_marker, subdomain_marker, physical_table = import_mesh(prefix='two-phase', subdomains=True, tdim=2, gdim=2, directory="mesh")
-# mesh = dolfin.UnitSquareMesh(16, 16)
 
 # define the symbols
 dx = dolfin.Measure('dx', domain=mesh, subdomain_data=subdomain_marker)
@@ -30,17 +29,18 @@ periodic_bc = PeriodicBoundary()
 # define the function spaces
 cell = mesh.ufl_cell()
 U = dolfin.VectorElement('CG', cell, degree=2)
-P = dolfin.FiniteElement('CG', cell, degree=1)
-TaylorHoodSpace = dolfin.FunctionSpace(mesh, dolfin.MixedElement([U, P]), constrained_domain = periodic_bc)
+P1 = dolfin.FiniteElement('CG', cell, degree=1)
+P0 = dolfin.FiniteElement('DG', cell, degree=0)
+AugTHSpace = dolfin.FunctionSpace(mesh, dolfin.MixedElement([U, P1, P0]), constrained_domain = periodic_bc)
 
 # define the flow boundary conditions
-noSlipBC_T = dolfin.DirichletBC(TaylorHoodSpace.sub(0), dolfin.Constant((0.0, 0.0)), \
+noSlipBC_T = dolfin.DirichletBC(AugTHSpace.sub(0), dolfin.Constant((0.0, 0.0)), \
                               boundary_marker, physical_table["top"])
-noPen_L = dolfin.DirichletBC(TaylorHoodSpace.sub(0).sub(1), dolfin.Constant(0.0), \
+noPen_L = dolfin.DirichletBC(AugTHSpace.sub(0).sub(1), dolfin.Constant(0.0), \
                                   boundary_marker, physical_table["dryleft"])
-noPen_W = dolfin.DirichletBC(TaylorHoodSpace.sub(0).sub(1), dolfin.Constant(0.0), \
+noPen_W = dolfin.DirichletBC(AugTHSpace.sub(0).sub(1), dolfin.Constant(0.0), \
                                   boundary_marker, physical_table["wet"])
-noPen_R = dolfin.DirichletBC(TaylorHoodSpace.sub(0).sub(1), dolfin.Constant(0.0), \
+noPen_R = dolfin.DirichletBC(AugTHSpace.sub(0).sub(1), dolfin.Constant(0.0), \
                                   boundary_marker, physical_table["dryright"])
 flow_bcs = [noSlipBC_T, noPen_L, noPen_W, noPen_R]
 
@@ -48,12 +48,13 @@ flow_bcs = [noSlipBC_T, noPen_L, noPen_W, noPen_R]
 phys = {"gamma":1.0, "kappa":-0.2}
 
 # define the variational problem
-u, p = dolfin.TrialFunctions(TaylorHoodSpace)
-v, q = dolfin.TestFunctions(TaylorHoodSpace)
-a = dolfin.inner(dolfin.grad(u), dolfin.grad(v)) * dx + dolfin.div(v) * p * dx + dolfin.div(u) * q * dx
+u, p1, p0 = dolfin.TrialFunctions(AugTHSpace)
+v, q1, q0 = dolfin.TestFunctions(AugTHSpace)
+# a = dolfin.inner(dolfin.grad(u), dolfin.grad(v)) * dx + dolfin.div(v) * p * dx + dolfin.div(u) * q * dx
+a = ( dolfin.inner(dolfin.grad(u), dolfin.grad(v)) + dolfin.div(v) * (p1+p0) + dolfin.div(u) * (q1+q0) ) * dx
 l = dolfin.Constant(phys["gamma"] * phys["kappa"]) * dolfin.dot(n('+'), v('+')) * dS # \
     # + dolfin.dot(dolfin.Constant((0.0, 0.0)), v) * dx # a hack to fix the interior facet normal
-b = dolfin.inner(dolfin.grad(u), dolfin.grad(v))*dx + p*q*dx
+b = dolfin.inner(dolfin.grad(u), dolfin.grad(v))*dx + (p1+p0)*(q1+q0)*dx # the preconditioning matrix
 
 # assemble the linear system
 A, L = dolfin.assemble_system(a, l, flow_bcs)
@@ -65,13 +66,16 @@ solver.set_operators(A, B)
 solver.parameters["report"] = True
 
 # create the null vector
-sol = dolfin.Function(TaylorHoodSpace)
-null_vec = dolfin.Vector(sol.vector())
-TaylorHoodSpace.sub(1).dofmap().set(null_vec, 1.0)
-null_vec *= 1.0 / null_vec.norm("l2")
+sol = dolfin.Function(AugTHSpace)
+nv1 = dolfin.Vector(sol.vector())
+nv0 = dolfin.Vector(sol.vector())
+AugTHSpace.sub(1).dofmap().set(nv1, 1.0)
+AugTHSpace.sub(2).dofmap().set(nv0, 1.0)
+nv1 *= 1.0 / nv1.norm("l2")
+nv0 *= 1.0 / nv0.norm("l2")
 
 # attach the null space to the PETSc matrix
-null_space = dolfin.VectorSpaceBasis([null_vec])
+null_space = dolfin.VectorSpaceBasis([nv1, nv0])
 dolfin.as_backend_type(A).set_nullspace(null_space)
 
 null_space.orthogonalize(L)
@@ -79,14 +83,20 @@ null_space.orthogonalize(L)
 # solve the linear system
 solver.solve(sol.vector(), L)
 
-u_sol, p_sol = sol.split()
+u_sol, p1_sol, p0_sol = sol.split()
 u_sol.rename("u", "u")
-p_sol.rename("p", "p")
+
+# Convert the P1+P0 pressure to a DG1 function
+PP = dolfin.FunctionSpace(mesh, dolfin.FiniteElement('DG', cell, degree=1), constrained_domain = periodic_bc)
+pp1 = dolfin.project(p1_sol, PP)
+pp0 = dolfin.project(p0_sol, PP)
+pp1.vector().axpy(1.0, pp0.vector())
+pp1.rename("p", "p")
 
 # output the solution
 outfile = dolfin.XDMFFile('data/two-phase-u.xdmf')
 outfile.write(u_sol)
 outfile.close()
 outfile = dolfin.XDMFFile('data/two-phase-p.xdmf')
-outfile.write(p_sol)
+outfile.write(pp1)
 outfile.close()
