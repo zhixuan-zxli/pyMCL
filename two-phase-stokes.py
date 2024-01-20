adab55e73e0a411e2d9605c830e71f81e56d5276
zhixuan-zxli <zihinlai@163.com>
2024-01-20 21:17:42 +0800

    Stokes problem with mixed function space (not mixed element); direct solver; looks OK.

--- two-phase-stokes.py
+++ two-phase-stokes.py
@@ -5,17 +5,20 @@ from msh2xdmf import import_mesh
 
 # load the mesh
 gdim = 2
-bulk_mesh, boundary_marker, subdomain_marker, physical_table = import_mesh(prefix='two-phase', subdomains=True, tdim=gdim, gdim=gdim, directory="mesh")
-interface_mesh = dolfin.MeshView.create(boundary_marker, physical_table["interface"])
-interface_mesh.init_cell_orientations(dolfin.Expression(("x[0] - 0.5", "x[1]"), degree = 1))
+# bulk_mesh, boundary_marker, subdomain_marker, physical_table = import_mesh(prefix='two-phase', subdomains=True, tdim=gdim, gdim=gdim, directory="mesh")
+# interface_mesh = dolfin.MeshView.create(boundary_marker, physical_table["interface"])
+# interface_mesh.init_cell_orientations(dolfin.Expression(("x[0] - 0.5", "x[1]"), degree = 1))
+bulk_mesh = dolfin.UnitSquareMesh(16, 16)
 
 # define the symbols
-dx = dolfin.Measure('dx', domain=bulk_mesh, subdomain_data=subdomain_marker)
-dS = dolfin.Measure('dx', domain=interface_mesh)
-n = dolfin.CellNormal(interface_mesh)
-print('Measure of interface as MeshView = {}'.format(dolfin.assemble(dolfin.Constant(1.0) * dS)))
-proj_mea = dolfin.dot(dolfin.Constant((0.0, 1.0)), n) * dS
-print('Projected measure = {}'.format(dolfin.assemble(proj_mea)))
+dx = dolfin.Measure('dx', domain=bulk_mesh)
+# dx = dolfin.Measure('dx', domain=bulk_mesh, subdomain_data=subdomain_marker)
+# dl = dolfin.Measure('ds', domain=bulk_mesh, subdomain_data=boundary_marker)
+# dS = dolfin.Measure('dx', domain=interface_mesh)
+# n = dolfin.CellNormal(interface_mesh)
+# print('Measure of interface as MeshView = {}'.format(dolfin.assemble(dolfin.Constant(1.0) * dS)))
+# proj_mea = dolfin.dot(dolfin.Constant((0.0, 1.0)), n) * dS
+# print('Projected measure = {}'.format(dolfin.assemble(proj_mea)))
 
 # Define the periodic boundary condition
 class PeriodicBoundary(dolfin.SubDomain):
@@ -28,43 +31,71 @@ class PeriodicBoundary(dolfin.SubDomain):
         y[1] = x[1]
 periodic_bc = PeriodicBoundary()
 
+# Define the endpoint of the interface
+def onMeshBoundary(x, on_boundary):
+    return on_boundary
+
 # define the function spaces
 FuncSpaces = dict()
 FuncSpaces["U"] = dolfin.VectorFunctionSpace(bulk_mesh, 'CG', 2, dim=gdim, constrained_domain = periodic_bc)
 FuncSpaces["P1"] = dolfin.FunctionSpace(bulk_mesh, 'CG', 1, constrained_domain = periodic_bc)
 FuncSpaces["P0"] = dolfin.FunctionSpace(bulk_mesh, 'DG', 0, constrained_domain = periodic_bc)
-FuncSpaces["X"] = dolfin.VectorFunctionSpace(interface_mesh, 'CG', 1, dim=gdim)
-FuncSpaces["K"] = dolfin.FunctionSpace(interface_mesh, 'CG', 1)
+# FuncSpaces["X"] = dolfin.VectorFunctionSpace(interface_mesh, 'CG', 1, dim=gdim)
+# FuncSpaces["K"] = dolfin.FunctionSpace(interface_mesh, 'CG', 1)
 FuncSpace = dolfin.MixedFunctionSpace(*FuncSpaces.values())
 
-
 # define the flow boundary conditions
-# noSlipBC_T = dolfin.DirichletBC(AugTHSpace.sub(0), dolfin.Constant((0.0, 0.0)), \
-#                               boundary_marker, physical_table["top"])
-# noPen_L = dolfin.DirichletBC(AugTHSpace.sub(0).sub(1), dolfin.Constant(0.0), \
-#                                   boundary_marker, physical_table["dryleft"])
-# noPen_W = dolfin.DirichletBC(AugTHSpace.sub(0).sub(1), dolfin.Constant(0.0), \
-#                                   boundary_marker, physical_table["wet"])
-# noPen_R = dolfin.DirichletBC(AugTHSpace.sub(0).sub(1), dolfin.Constant(0.0), \
-#                                   boundary_marker, physical_table["dryright"])
-# flow_bcs = [noSlipBC_T, noPen_L, noPen_W, noPen_R]
+noSlipBC = dolfin.DirichletBC(FuncSpace.sub_space(0), dolfin.Constant((0.0, 0.0)), onMeshBoundary)
+# noSlipBC = dolfin.DirichletBC(FuncSpace.sub_space(0), dolfin.Constant((0.0, 0.0)), boundary_marker, physical_table["top"])
+# noPenBC = dolfin.DirichletBC(FuncSpace.sub_space(0).sub(1), dolfin.Constant(0.0), boundary_marker, physical_table["bottom"])
+# define the attach boundary condition
+# attachBC = dolfin.DirichletBC(FuncSpace.sub_space(3).sub(1), dolfin.Constant(0.0), onMeshBoundary)
+
+essentialBCs = [noSlipBC] #, noPenBC] #, attachBC]
 
 # define the physical parameters
-# phys = {"gamma":1.0, "kappa":-0.2}
+phys = {"Re":1.0, "Ca":0.1, "ls":0.1, "beta":0.1}
+
+f = dolfin.Expression(("-2*pi*pi*sin(2*pi*x[1])*(1-2*sin(pi*x[0]))*(1+2*sin(pi*x[0]))", \
+                       "-2*pi*pi*sin(2*pi*x[0])*(2*sin(pi*x[1])+1)*(2*sin(pi*x[1])-1)"), degree=3)
+
 
 # define the variational problem
-# u, p1, p0 = dolfin.TrialFunctions(AugTHSpace)
-# v, q1, q0 = dolfin.TestFunctions(AugTHSpace)
-# a = ( dolfin.inner(dolfin.grad(u), dolfin.grad(v)) + dolfin.div(v) * (p1+p0) + dolfin.div(u) * (q1+q0) ) * dx
+# (u, p1, p0, x, kappa) = dolfin.TrialFunctions(FuncSpace)
+# (v, q1, q0, y, chi) = dolfin.TestFunctions(FuncSpace)
+(u, p1, p0) = dolfin.TrialFunctions(FuncSpace)
+(v, q1, q0) = dolfin.TestFunctions(FuncSpace)
+a = dolfin.Constant(1.0/phys["Re"]) * dolfin.inner(dolfin.grad(u), dolfin.grad(v)) * dx - dolfin.div(v) * (p1+p0) * dx + dolfin.div(u) * (q1+q0) * dx 
+l = dolfin.dot(f, v) * dx
+# l = dolfin.Constant(0.0) * (q1 + q0) * dx
+#   + dolfin.Constant(phys["beta"]/phys["ls"]) * u[0] * v[0] * dl(physical_table["bottom"])
+#   + dolfin.Constant(1.0/phys["Ca"]) * kappa * dolfin.dot(n, v) * dS
 # l = dolfin.Constant(phys["gamma"] * phys["kappa"]) * dolfin.dot(n('+'), v('+')) * dS # \
 #     # + dolfin.dot(dolfin.Constant((0.0, 0.0)), v) * dx # a hack to fix the interior facet normal
 # b = dolfin.inner(dolfin.grad(u), dolfin.grad(v))*dx + (p1+p0)*(q1+q0)*dx # the preconditioning matrix
 
 # assemble the linear system
+sol = dolfin.Function(FuncSpace)
+# dolfin.solve(a == l, sol, essentialBCs)
+system = dolfin.assemble_mixed_system(a == l, sol, essentialBCs)
+matrix_blocks = system[0]
+rhs_blocks = system[1]
 # A, L = dolfin.assemble_system(a, l, flow_bcs)
 # B, _ = dolfin.assemble_system(b, l, flow_bcs)
 
+A = dolfin.PETScNestMatrix(matrix_blocks)
+L = dolfin.Vector()
+A.init_vectors(L, rhs_blocks)
+A.convert_to_aij()
+
 # set up the solver
+sol_vec = dolfin.Vector()
+solver = dolfin.LUSolver()
+solver.solve(A, sol_vec, L)
+FuncSpaceDims = [fs.dim() for fs in FuncSpaces.values()]
+u_sol = dolfin.Function(FuncSpaces["U"])
+u_sol.vector().set_local(sol_vec.get_local()[:FuncSpaceDims[0]])
+# (u, p1, p0) = sol.split()
 # solver = dolfin.KrylovSolver("gmres", "amg")
 # solver.set_operators(A, B)
 # solver.parameters["report"] = True
@@ -90,6 +121,6 @@ FuncSpace = dolfin.MixedFunctionSpace(*FuncSpaces.values())
 
 
 # output the solution
-# outfile = dolfin.XDMFFile('data/two-phase-u.xdmf')
-# outfile.write(u_sol)
-# outfile.close()
+outfile = dolfin.XDMFFile('data/two-phase-u.xdmf')
+outfile.write(u_sol)
+outfile.close()
