eb56d3db49271f626091eeaf64899c960d9acc94
zhixuan-zxli <zihinlai@163.com>
2024-01-22 11:58:46 +0800

    Testing convergence of Stokes.

--- two-phase-stokes.py
+++ two-phase-stokes.py
@@ -3,111 +3,148 @@ from dolfin import *
 from msh2xdmf import import_mesh
 # from matplotlib import pyplot
 
-# load the mesh
-gdim = 2
-bulk_mesh = UnitSquareMesh(16, 16)
+mesh_table = [8, 16, 32]
+error_table = {"u, L2":[], 
+               "u, Linf":[]}
 
-# define the symbols
-dx = Measure('dx', domain=bulk_mesh)
+def printConvergenceTable(mesh_table, error_table):
+    m = len(mesh_table)
+    # print the header
+    header_str = "\n{0: <20}".format("")
+    for i in range(m-1):
+        header_str += "{0: <16}{1: <16}".format(mesh_table[i], "rate")
+    header_str += "{0: <16}".format(mesh_table[-1])
+    print(header_str)
+    # print each norm
+    for (norm_type, error_list) in error_table.items():
+        error_str = "{0: <20}".format(norm_type)
+        for i in range(m-1):
+            error_str += "{0:<16.2e}{1:<16.2f}".format(error_list[i], np.log2(error_list[i]/error_list[i+1]))
+        error_str += "{0:<16.2e}".format(error_list[-1])
+        print(error_str)
 
-# Define the periodic boundary condition
-class PeriodicBoundary(SubDomain):
-    # identify the left boundary
-    def inside(self, x, on_boundary):
-        return on_boundary and near(x[0], 0.0)
-    # map the right boundary (x) to the left (y)
-    def map(self, x, y):
-        y[0] = x[0] - 1.0
-        y[1] = x[1]
-periodic_bc = PeriodicBoundary()
+def testStokes(mesh_div):
+    # load the mesh
+    bulk_mesh = UnitSquareMesh(mesh_div, mesh_div)
 
-# Define the endpoint of the interface
-def onMeshBoundary(x, on_boundary):
-    return on_boundary
+    # define the symbols
+    dx = Measure('dx', domain=bulk_mesh)
 
-# define the function spaces
-FuncSpaces = dict()
-FuncSpaces["U"] = VectorFunctionSpace(bulk_mesh, 'CG', 2, dim=gdim, constrained_domain = periodic_bc)
-FuncSpaces["P1"] = FunctionSpace(bulk_mesh, 'CG', 1, constrained_domain = periodic_bc)
-FuncSpaces["P0"] = FunctionSpace(bulk_mesh, 'DG', 0, constrained_domain = periodic_bc)
-FuncSpace = MixedFunctionSpace(*FuncSpaces.values())
+    # Define the periodic boundary condition
+    class PeriodicBoundary(SubDomain):
+        # identify the left boundary
+        def inside(self, x, on_boundary):
+            return on_boundary and near(x[0], 0.0)
+        # map the right boundary (x) to the left (y)
+        def map(self, x, y):
+            y[0] = x[0] - 1.0
+            y[1] = x[1]
+    periodic_bc = PeriodicBoundary()
 
-# define the flow boundary conditions
-noSlipBC = DirichletBC(FuncSpace.sub_space(0), Constant((0.0, 0.0)), onMeshBoundary)
+    # Define the endpoint of the interface
+    def onMeshBoundary(x, on_boundary):
+        return on_boundary
 
-essentialBCs = [noSlipBC] #, noPenBC] #, attachBC]
+    # define the function spaces
+    FuncSpaces = dict()
+    FuncSpaces["U"] = VectorFunctionSpace(bulk_mesh, 'CG', 2, constrained_domain = periodic_bc)
+    FuncSpaces["P1"] = FunctionSpace(bulk_mesh, 'CG', 1, constrained_domain = periodic_bc)
+    FuncSpaces["P0"] = FunctionSpace(bulk_mesh, 'DG', 0, constrained_domain = periodic_bc)
+    FuncSpace = MixedFunctionSpace(*FuncSpaces.values())
 
-# define the physical parameters
-phys = {"Re":1.0, "Ca":0.1, "ls":0.1, "beta":0.1}
+    # define the flow boundary conditions
+    noSlipBC = DirichletBC(FuncSpace.sub_space(0), Constant((0.0, 0.0)), onMeshBoundary)
 
-f = Expression(("-2*pi*pi*sin(2*pi*x[1])*(1-2*sin(pi*x[0]))*(1+2*sin(pi*x[0]))", \
-                       "-2*pi*pi*sin(2*pi*x[0])*(2*sin(pi*x[1])+1)*(2*sin(pi*x[1])-1)"), degree=3)
+    essentialBCs = [noSlipBC] #, noPenBC] #, attachBC]
 
+    # define the physical parameters
+    phys = {"Re":1.0, "Ca":0.1, "ls":0.1, "beta":0.1}
 
-# define the variational problem
-(u, p1, p0) = TrialFunctions(FuncSpace)
-(v, q1, q0) = TestFunctions(FuncSpace)
-a = Constant(1.0/phys["Re"]) * inner(grad(u), grad(v)) * dx + div(v) * (p1+p0) * dx + div(u) * (q1+q0) * dx 
-l = dot(f, v) * dx
-b = Constant(0.0) * inner(u, v) * dx + (p1+p0)*(q1+q0)*dx # the preconditioning matrix
+    f = Expression(("-2*pi*pi*sin(2*pi*x[1])*(1-2*sin(pi*x[0]))*(1+2*sin(pi*x[0])) - pi*exp(pi*x[0])*cos(pi*x[1])", \
+                        "-2*pi*pi*sin(2*pi*x[0])*(2*sin(pi*x[1])+1)*(2*sin(pi*x[1])-1) + pi*exp(pi*x[0])*sin(pi*x[1])"), degree=3)
 
-# assemble the linear system
-sol = Function(FuncSpace)
-u_sol = Function(FuncSpaces["U"])
-p1_sol = Function(FuncSpaces["P1"])
-p0_sol = Function(FuncSpaces["P0"])
+    u_exact = Expression(("sin(pi*x[0])*sin(pi*x[0])*sin(2*pi*x[1])", "-sin(2*pi*x[0])*sin(pi*x[1])*sin(pi*x[1])"), degree=3)
+    p_exact = Expression("exp(pi*x[0])*sin(pi*x[1])", degree=2)
 
-system = assemble_mixed_system(a == l, sol, essentialBCs)
-A_blocks = system[0]
-rhs_blocks = system[1]
+    # define the variational problem
+    (u, p1, p0) = TrialFunctions(FuncSpace)
+    (v, q1, q0) = TestFunctions(FuncSpace)
+    a = Constant(1.0/phys["Re"]) * inner(grad(u), grad(v)) * dx + div(v) * (p1+p0) * dx + div(u) * (q1+q0) * dx 
+    l = dot(f, v) * dx
+    b = Constant(0.0) * inner(u, v) * dx + (p1+p0)*(q1+q0)*dx # the preconditioning matrix
 
-A = PETScNestMatrix(A_blocks)
-L = Vector()
-sol_vec = Vector()
-A.init_vectors(L, rhs_blocks)
-A.init_vectors(sol_vec, [u_sol.vector(), p1_sol.vector(), p0_sol.vector()])
+    # assemble the linear system
+    sol = Function(FuncSpace)
+    u_sol = Function(FuncSpaces["U"])
+    p1_sol = Function(FuncSpaces["P1"])
+    p0_sol = Function(FuncSpaces["P0"])
 
-system = assemble_mixed_system(b == l, sol, essentialBCs)
-B_blocks = system[0]
-B_blocks[0] = A_blocks[0]
-B = PETScNestMatrix(B_blocks)
+    system = assemble_mixed_system(a == l, sol, essentialBCs)
+    A_blocks = system[0]
+    rhs_blocks = system[1]
 
-# set up the preconditioner
+    A = PETScNestMatrix(A_blocks)
+    L = Vector()
+    sol_vec = Vector()
+    A.init_vectors(L, rhs_blocks)
+    A.init_vectors(sol_vec, [u_sol.vector(), p1_sol.vector(), p0_sol.vector()])
 
-# PETScOptions.set("ksp_view");
-# PETScOptions.set("ksp_monitor_true_residual");
-PETScOptions.set("pc_type", "fieldsplit");
-PETScOptions.set("pc_fieldsplit_type", "additive");
+    A.convert_to_aij()
 
-PETScOptions.set("fieldsplit_0_ksp_type", "preonly");
-PETScOptions.set("fieldsplit_0_pc_type", "gamg");
-PETScOptions.set("fieldsplit_1_ksp_type", "preonly");
-PETScOptions.set("fieldsplit_1_pc_type", "lu");
-PETScOptions.set("fieldsplit_2_ksp_type", "preonly");
-PETScOptions.set("fieldsplit_2_pc_type", "lu");
+    # system = assemble_mixed_system(b == l, sol, essentialBCs)
+    # B_blocks = system[0]
+    # B_blocks[0] = A_blocks[0]
+    # B = PETScNestMatrix(B_blocks)
 
-# solve the linear system
-solver = PETScKrylovSolver("minres")
-solver.set_from_options()
-solver.set_operators(A, B)
-prms = solver.parameters
-prms["monitor_convergence"] = True
-# prms["relative_tolerance"] = 1e-5
+    # set up the preconditioner
 
-fields = [PETScNestMatrix.get_block_dofs(A, i) for i in range(3)]
-PETScPreconditioner.set_fieldsplit(solver, fields, ["0", "1", "2"])
+    # PETScOptions.set("ksp_view");
+    # PETScOptions.set("ksp_monitor_true_residual");
+    # PETScOptions.set("pc_type", "fieldsplit");
+    # PETScOptions.set("pc_fieldsplit_type", "additive");
 
-solver.solve(sol_vec, L)
-sol_vec.apply("")
+    # PETScOptions.set("fieldsplit_0_ksp_type", "preonly");
+    # PETScOptions.set("fieldsplit_0_pc_type", "gamg");
+    # PETScOptions.set("fieldsplit_1_ksp_type", "preonly");
+    # PETScOptions.set("fieldsplit_1_pc_type", "lu");
+    # PETScOptions.set("fieldsplit_2_ksp_type", "preonly");
+    # PETScOptions.set("fieldsplit_2_pc_type", "lu");
 
-# get the sub solution
-FuncSpaceDims = [fs.dim() for fs in FuncSpaces.values()]
-u_sol.vector().set_local(sol_vec.get_local()[:FuncSpaceDims[0]])
-u_sol.vector().apply("")
+    # solve the linear system
+    # solver = PETScKrylovSolver("gmres")
+    # solver.set_from_options()
+    # solver.set_operators(A, B)
+    # prms = solver.parameters
+    # prms["monitor_convergence"] = True
+    # prms["relative_tolerance"] = 1e-5
+    # prms["report"] = True
 
-# Convert the P1+P0 pressure to a DG1 function
+    # fields = [PETScNestMatrix.get_block_dofs(A, i) for i in range(3)]
+    # PETScPreconditioner.set_fieldsplit(solver, fields, ["0", "1", "2"])
 
-# output the solution
-outfile = XDMFFile('data/two-phase-u.xdmf')
-outfile.write(u_sol)
-outfile.close()
+    # solver.solve(sol_vec, L)
+    solve(A, sol_vec, L) # LU solver
+    sol_vec.apply("")
+
+    # get the sub solution
+    FuncSpaceDims = np.array([fs.dim() for fs in FuncSpaces.values()])
+    FuncSpaceDims = np.cumsum(FuncSpaceDims)
+    u_sol.vector().set_local(sol_vec.get_local()[:FuncSpaceDims[0]])
+    u_sol.vector().apply("")
+    p1_sol.vector().set_local(sol_vec.get_local()[FuncSpaceDims[0]:FuncSpaceDims[1]])
+    p1_sol.vector().apply("")
+    p0_sol.vector().set_local(sol_vec.get_local()[FuncSpaceDims[1]:])
+    p0_sol.vector().apply("")
+
+    # Convert the P1+P0 pressure to a DG1 function
+
+    # calculate the errors
+    error_table["u, L2"].append(errornorm(u_exact, u_sol, "L2"))
+    u_err = interpolate(u_exact, FuncSpaces["U"])
+    u_err.vector().axpy(-1.0, u_sol.vector())
+    u_err.vector().apply("")
+    error_table["u, Linf"].append(np.linalg.norm(u_err.vector().get_local(), np.inf))
+
+for i in mesh_table:
+    testStokes(i)
+printConvergenceTable(mesh_table, error_table)
