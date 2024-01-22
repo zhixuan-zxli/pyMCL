2ba42cfc8a7d632697e7618ec84f38b3c23a4b12
zhixuan-zxli <zihinlai@163.com>
2024-01-22 13:49:25 +0800

    Solving Stokes using P2/(P1+P0) element looks perfect.

--- two-phase-stokes.py
+++ two-phase-stokes.py
@@ -3,25 +3,35 @@ from dolfin import *
 from msh2xdmf import import_mesh
 # from matplotlib import pyplot
 
-mesh_table = [8, 16, 32]
+mesh_table = [8, 16, 32, 64]
 error_table = {"u, L2":[], 
-               "u, Linf":[]}
+               "u, H10":[], 
+               "u, Linf":[], 
+               "p, L2":[], 
+               "p, Linf":[]}
 
 def printConvergenceTable(mesh_table, error_table):
     m = len(mesh_table)
     # print the header
     header_str = "\n{0: <20}".format("")
     for i in range(m-1):
-        header_str += "{0: <16}{1: <16}".format(mesh_table[i], "rate")
-    header_str += "{0: <16}".format(mesh_table[-1])
+        header_str += "{0: <10}{1: <8}".format(mesh_table[i], "rate")
+    header_str += "{0: <10}".format(mesh_table[-1])
     print(header_str)
     # print each norm
     for (norm_type, error_list) in error_table.items():
         error_str = "{0: <20}".format(norm_type)
         for i in range(m-1):
-            error_str += "{0:<16.2e}{1:<16.2f}".format(error_list[i], np.log2(error_list[i]/error_list[i+1]))
-        error_str += "{0:<16.2e}".format(error_list[-1])
+            error_str += "{0:<10.2e}{1:<8.2f}".format(error_list[i], np.log2(error_list[i]/error_list[i+1]))
+        error_str += "{0:<10.2e}".format(error_list[-1])
         print(error_str)
+
+def zeroMean(p, dx):
+    p_avr = assemble(p*dx) / assemble(Constant(1.0) * dx)
+    p_vec = p.vector().get_local() - p_avr
+    p.vector().set_local(p_vec)
+    p.vector().apply("")
+    return p
 
 def testStokes(mesh_div):
     # load the mesh
@@ -60,18 +70,18 @@ def testStokes(mesh_div):
     # define the physical parameters
     phys = {"Re":1.0, "Ca":0.1, "ls":0.1, "beta":0.1}
 
-    f = Expression(("-2*pi*pi*sin(2*pi*x[1])*(1-2*sin(pi*x[0]))*(1+2*sin(pi*x[0])) - pi*exp(pi*x[0])*cos(pi*x[1])", \
-                        "-2*pi*pi*sin(2*pi*x[0])*(2*sin(pi*x[1])+1)*(2*sin(pi*x[1])-1) + pi*exp(pi*x[0])*sin(pi*x[1])"), degree=3)
+    f = Expression(("-2*pi*pi*sin(2*pi*x[1])*(1-2*sin(pi*x[0]))*(1+2*sin(pi*x[0])) + 2*pi*sin(2*pi*x[0])*exp(pi*x[1])", \
+                        "-2*pi*pi*sin(2*pi*x[0])*(2*sin(pi*x[1])+1)*(2*sin(pi*x[1])-1) - pi * cos(2*pi*x[0]) * exp(pi*x[1])"), degree=3)
 
     u_exact = Expression(("sin(pi*x[0])*sin(pi*x[0])*sin(2*pi*x[1])", "-sin(2*pi*x[0])*sin(pi*x[1])*sin(pi*x[1])"), degree=3)
-    p_exact = Expression("exp(pi*x[0])*sin(pi*x[1])", degree=2)
+    p_exact = Expression("cos(2*pi*x[0]) * exp(pi*x[1])", degree=3)
 
     # define the variational problem
     (u, p1, p0) = TrialFunctions(FuncSpace)
     (v, q1, q0) = TestFunctions(FuncSpace)
     a = Constant(1.0/phys["Re"]) * inner(grad(u), grad(v)) * dx + div(v) * (p1+p0) * dx + div(u) * (q1+q0) * dx 
     l = dot(f, v) * dx
-    b = Constant(0.0) * inner(u, v) * dx + (p1+p0)*(q1+q0)*dx # the preconditioning matrix
+    # b = Constant(0.0) * inner(u, v) * dx + (p1+p0)*(q1+q0)*dx # the preconditioning matrix
 
     # assemble the linear system
     sol = Function(FuncSpace)
@@ -136,15 +146,35 @@ def testStokes(mesh_div):
     p0_sol.vector().set_local(sol_vec.get_local()[FuncSpaceDims[1]:])
     p0_sol.vector().apply("")
 
-    # Convert the P1+P0 pressure to a DG1 function
-
     # calculate the errors
     error_table["u, L2"].append(errornorm(u_exact, u_sol, "L2"))
-    u_err = interpolate(u_exact, FuncSpaces["U"])
+    error_table["u, H10"].append(errornorm(u_exact, u_sol, "H10"))
+    u_err = project(u_exact, FuncSpaces["U"])
     u_err.vector().axpy(-1.0, u_sol.vector())
     u_err.vector().apply("")
     error_table["u, Linf"].append(np.linalg.norm(u_err.vector().get_local(), np.inf))
 
+    # Convert the P1+P0 pressure to a DG1 function
+    DG1_space = FunctionSpace(bulk_mesh, 'DG', 1)
+    p1_proj = project(p1_sol, DG1_space)
+    p0_proj = project(p0_sol, DG1_space)
+    p1_proj.vector().axpy(1.0, p0_proj.vector())
+    p1_proj.vector().apply("")
+    zeroMean(p1_proj, dx)
+
+    # with XDMFFile("data/two-phase-p.xdmf") as outfile:
+    #     outfile.write(p1_proj)
+
+    # calculate the errors for the pressure
+    p_err = project(p_exact, DG1_space)
+    p_err.vector().axpy(-1.0, p1_proj.vector())
+    p_err.vector().apply("")
+    zeroMean(p_err, dx)
+    error_table["p, L2"].append(np.sqrt(assemble(p_err**2*dx)))
+    error_table["p, Linf"].append(np.linalg.norm(p_err.vector().get_local(), np.inf))
+
+
 for i in mesh_table:
+    print('Testing level {}...'.format(i))
     testStokes(i)
 printConvergenceTable(mesh_table, error_table)
