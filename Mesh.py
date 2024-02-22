d16dc66477e3280dcfe5582e5bb0939b33ecc6aa
zhixuan-zxli <zihinlai@163.com>
2024-02-22 19:44:29 +0800

    Wrote BuildP2Mesh.

--- Mesh.py
+++ Mesh.py
@@ -1,8 +1,12 @@
 import meshio
 import numpy as np
+from scipy.sparse import csr_matrix
 
 class Mesh:
     def __init__(self, mesh_name: str) -> None:
+        """
+        Load a GMSH mesh. 
+        """
         msh = meshio.read(mesh_name)
         self.Np = [msh.points.shape[0]]
         # 1. read the points
@@ -24,11 +28,41 @@ class Mesh:
             else:
                 data = np.zeros((cell.data.shape[0], 1), dtype=np.uint32)
             if cell.type == "vertex" and data != None:
-                self.point[cell.data, :] = data
+                self.point[cell.data, -1] = data
             elif cell.type == "line":
                 self.edge = np.vstack((self.edge, np.hstack((cell.data, data))))
             elif cell.type == "triangle":
                 self.tri = np.vstack((self.tri, np.hstack((cell.data, data))))
             else:
                 raise RuntimeError("Unrecognized cell type. ")
+            
+    def isP2Mesh(self) -> bool:
+        return len(self.Np) == 2
+
+    def buildP2Mesh(self) -> None:
+        """
+        Build the P2 mesh. 
+        """
+        assert(not self.isP2Mesh())
+        Np = self.Np[0]
+        T = self.tri
+        Nt = T.shape[0]
+        tri_edges = np.block([ [T[:,[0]], T[:,[1]]], [T[:,[2]], T[:,[3]]], [T[:,[2]], T[:,[0]]] ])
+        S = csr_matrix((np.ones((3*Nt,)), (np.min(tri_edges, axis=1), np.max(tri_edges, axis=1))), shape=(Np, Np))
+        S.data = np.arange(1, S.nnz+1, dtype=S.dtype)
+        row, col = S.nonzero()
+        # build new nodes
+        new_points = 0.5 * (self.point[row, :] + self.point[col, :])
+        new_points[:, -1] = 0. # reset the tags
+        self.point = np.vstack((self.point, new_points))
+        self.Np += [self.point.shape[0]]
+        # update edges
+        self.edge = np.hstack((self.edge, self.edge[:, [-1]]))
+        self.edge[:,-2] = S[np.min(self.edge[:,0:2], axis=1), np.max(self.edge[:,0:2], axis=1)]
+        # update the triangles
+        self.tri = np.hstack((self.tri[:,0:3], np.zeros((Nt, 3), dtype=self.tri.dtype), self.tri[:,[-1]]))
+        self.tri[:,3] = S[np.min(self.tri[:,0:2], axis=1), np.max(self.tri[:,0:2], axis=1)]
+        self.tri[:,4] = S[np.min(self.tri[:,1:3], axis=1), np.max(self.tri[:,1:3], axis=1)]
+        self.tri[:,5] = S[np.min(self.tri[:,[0,2]], axis=1), np.max(self.tri[:,[0,2]], axis=1)]
+
 
--- test.py
+++ test.py
@@ -2,4 +2,5 @@ import numpy as np
 from Mesh import Mesh
 
 mesh = Mesh("mesh/two-phase.msh")
-pass
+mesh.buildP2Mesh()
+print("Finished.")
