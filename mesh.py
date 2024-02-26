3943d11108bda182c3605e151940153ccc674699
zhixuan-zxli <zihinlai@163.com>
2024-02-26 10:28:21 +0800

    Add tag field in Mesh.

--- mesh.py
+++ mesh.py
@@ -6,10 +6,14 @@ from matplotlib.pyplot import triplot, axis
 
 class Mesh:
     def __init__(self) -> None:
-        self.point = np.zeros((0, 4), dtype=np.float64)
-        self.edge = np.zeros((0, 3), dtype=np.uint32)
-        self.tri = np.zeros((0, 4), dtype=np.uint32)
-        self.tet = np.zeros((0, 5), dtype=np.uint32)
+        self.point = None
+        self.point_tag = None
+        self.edge = np.zeros((0, 2), dtype=np.uint32)
+        self.tri = np.zeros((0, 3), dtype=np.uint32)
+        # self.quad = np.zeros((0, 4), dtype=np.uint32)
+        # self.tet = np.zeros((0, 4), dtype=np.uint32)
+        self.edge_tag = np.zeros((0, ), dtype=np.uint32)
+        self.tri_tag = np.zeros((0, ), dtype=np.uint32)
 
     def load(self, mesh_name: str) -> None:
         """
@@ -20,7 +24,7 @@ class Mesh:
         self.point = msh.points
         if np.all(self.point[:,-1] == 0.): 
             self.point = self.point[:,:-1] # Remove the z coordinates if this is a planar mesh
-        self.point = np.hstack((self.point, np.zeros((self.point.shape[0], 1)))) # append the tags
+        self.point_tag = np.zeros((self.point.shape[0],), dtype=np.uint32)
 
         # 2. read the higher-dimensional entities
         num_cells = len(msh.cells)
@@ -29,15 +33,17 @@ class Mesh:
         for cid in range(num_cells):
             cell = msh.cells[cid]
             if has_physical:
-                data = msh.cell_data["gmsh:physical"][cid].astype(np.uint32).reshape(-1, 1) # reshape to a column vector
+                data = msh.cell_data["gmsh:physical"][cid]
             else:
-                data = np.zeros((cell.data.shape[0], 1), dtype=np.uint32)
+                data = np.zeros((cell.data.shape[0],), dtype=np.uint32)
             if cell.type == "vertex" and data != None:
-                self.point[cell.data, -1] = data
+                self.point_tag[cell.data] = data
             elif cell.type == "line":
-                self.edge = np.vstack((self.edge, np.hstack((cell.data.astype(np.uint32), data))))
+                self.edge = np.vstack((self.edge, cell.data))
+                self.edge_tag = np.hstack((self.edge_tag, data))
             elif cell.type == "triangle":
-                self.tri = np.vstack((self.tri, np.hstack((cell.data.astype(np.uint32), data))))
+                self.tri = np.vstack((self.tri, cell.data))
+                self.tri_tag = np.hstack((self.tri_tag, data))
             else:
                 raise RuntimeError("Unrecognized cell type. ")
 
@@ -46,30 +52,32 @@ class Mesh:
         submesh = Mesh()
         if sub_dim == 0:
             # extract the points
-            point_flag = np.zeros((Np,), dtype=np.bool8)
+            point_mask = np.zeros_like(self.point_tag, dtype=np.bool8)
             for t in tags:
-                point_flag[self.point[:,-1] == t] = True
-            submesh.point_map = np.arange(0, Np)[point_flag]
-            submesh.point = self.point[point_flag, :]
+                point_mask[self.point_tag == t] = True
+            submesh.point_map = np.arange(0, Np)[point_mask]
+            submesh.point = self.point[point_mask]
+            submesh.point_tag = self.point_tag[point_mask]
         elif sub_dim == 1:
             # extract the edges
-            edge_flag = np.zeros((self.edge.shape[0],), dtype=np.bool8)
+            edge_mask = np.zeros_like(self.edge_tag, dtype=np.bool8)
             for t in tags:
-                edge_flag[self.edge[:,-1] == t] = True
-            submesh.edge_map = np.arange(0, self.edge.shape[0], dtype=np.uint32)[edge_flag]
-            submesh.edge = self.edge[edge_flag, :]
+                edge_mask[self.edge_tag == t] = True
+            submesh.edge_map = np.arange(0, self.edge.shape[0], dtype=np.uint32)[edge_mask]
+            submesh.edge = self.edge[edge_mask, :]
+            submesh.edge_tag = self.edge_tag[edge_mask, :]
             # extract the points
-            point_flag = np.zeros((Np,), dtype=np.bool8)
-            point_flag[submesh.edge[:,0]] = True
-            point_flag[submesh.edge[:,1]] = True
-            submesh.point_map = np.arange(0, Np, dtype=np.uint32)[point_flag]
-            submesh.point = self.point[point_flag]
+            point_mask = np.zeros((Np,), dtype=np.bool8)
+            point_mask[submesh.edge.flatten()] = True
+            submesh.point_map = np.arange(0, Np, dtype=np.uint32)[point_mask]
+            submesh.point = self.point[point_mask]
+            submesh.point_tag = self.point_tag[point_mask]
             # finally fix the edge indices
-            inv_point_map = np.cumsum(point_flag) - 1
-            submesh.edge[:,0] = inv_point_map[submesh.edge[:,0]]
-            submesh.edge[:,1] = inv_point_map[submesh.edge[:,1]]
+            inv_point_map = np.cumsum(point_mask) - 1
+            submesh.edge = inv_point_map[submesh.edge.flatten()]
+            submesh.edge = submesh.edge.reshape(-1, 2)
         else:
-            raise RuntimeError("Submesh of dimension {} not implemented".format(sub_dim))
+            raise NotImplementedError
         return submesh
     
     def draw(self) -> None:
