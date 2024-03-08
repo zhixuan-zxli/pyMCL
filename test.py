a883f5617fb20fb6d1ed30cae679510912684f96
Zhixuan Li <zihinlai@163.com>
2024-03-08 12:32:50 +0800

    Poisson P2 with Dirichlet condition looks good.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from fe import Measure, TriP1, TriP2
 from function import Function
 from assemble import assembler, Form, setMeshMapping
 from scipy.sparse.linalg import spsolve
-# from matplotlib import pyplot
+from matplotlib import pyplot
 
 def exact(x, y) -> np.ndarray:
     return np.sin(np.pi*x) * np.cos(y)
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     mesh = Mesh()
     mesh.load("mesh/unit_square.msh")
     setMeshMapping(mesh)
-    u_space = TriP1(mesh)
+    u_space = TriP2(mesh)
     asm = assembler(u_space, u_space, Measure(2, None), 3)
     f = asm.linear(Form(rhs, "f"))
     A = asm.bilinear(Form(a, "grad"))
@@ -37,7 +37,8 @@ if __name__ == "__main__":
     fdof = np.ones((u_space.num_dof, ), dtype=np.bool8)
     fdof[bdof] = False
     # interpolate
-    u_exact = exact(mesh.point[:,0], mesh.point[:,1]).reshape(-1, 1) # col vector
+    u_exact = exact(u_space.dofloc[:, 0], u_space.dofloc[:, 1]).reshape(-1, 1) # col vector
+    # u_exact = exact(mesh.point[:,0], mesh.point[:,1]).reshape(-1, 1) # col vector
     u = Function(u_space)
     u[bdof] = u_exact[bdof]
     # homogeneize
@@ -48,3 +49,5 @@ if __name__ == "__main__":
     u[fdof, 0] = u_sol
     u_err = u - u_exact
     print("error = {0:.3e}".format(np.linalg.norm(u_err, ord=np.inf)))
+
+    # test boundary integral
