6de62c2cc6a11539edcaa0ddb925a3c66683a4f0
Zhixuan Li <zihinlai@163.com>
2024-03-10 17:39:15 +0800

    Tested Poisson problem with Neumann condition; looks good but convergence not tested.

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ from matplotlib import pyplot
 def exact(x, y) -> np.ndarray:
     return np.sin(np.pi*x) * np.cos(y)
 
+def exact_grad(x, y) -> np.ndarray:
+    return np.array((np.pi * np.cos(np.pi*x) * np.cos(y), -np.sin(np.pi*x) * np.sin(y)))
+
 def rhs(psi, coord) -> np.ndarray:
     # psi: (1,1,Nq)
     # coord: (2,Ne,Nq)
@@ -23,31 +26,39 @@ def a(phi, psi, coord) -> np.ndarray:
     data = data * coord.dx
     return data[:,np.newaxis]
 
+def bc(psi, coord) -> np.ndarray:
+    x, y = coord[0], coord[1] # (Ne, Nq)
+    ngrad = np.sum(exact_grad(x, y) * coord.n, axis=0, keepdims=True) # (1, Ne, Nq)
+    return ngrad * psi * coord.dx
+
+def integral(coord, u) -> np.ndarray:
+    return u * coord.dx
+
+
 if __name__ == "__main__":
     mesh = Mesh()
     mesh.load("mesh/unit_square.msh")
     setMeshMapping(mesh)
-    u_space = TriP2(mesh)
-    asm = assembler(u_space, u_space, Measure(2, None), 3)
-    f = asm.linear(Form(rhs, "f"))
-    A = asm.bilinear(Form(a, "grad"))
-    # collect boundary dofs
-    bdof = u_space.getCellDof(Measure(1, (2,)))
-    bdof = np.unique(bdof)
-    fdof = np.ones((u_space.num_dof, ), dtype=np.bool8)
-    fdof[bdof] = False
-    # interpolate
-    u_exact = exact(u_space.dofloc[:, 0], u_space.dofloc[:, 1]).reshape(-1, 1) # col vector
-    # u_exact = exact(mesh.point[:,0], mesh.point[:,1]).reshape(-1, 1) # col vector
-    u = Function(u_space)
-    u[bdof] = u_exact[bdof]
-    # homogeneize
-    f = f - A @ u
-    # solve
-    u_sol = spsolve(A[fdof][:,fdof], f[fdof])
-    # calculate error
-    u[fdof, 0] = u_sol
-    u_err = u - u_exact
-    print("error = {0:.3e}".format(np.linalg.norm(u_err, ord=np.inf)))
-
-    # test boundary integral
+    u_space = TriP1(mesh)
+    asm_2 = assembler(u_space, u_space, Measure(2, None), 3)
+    f = asm_2.linear(Form(rhs, "f"))
+    A = asm_2.bilinear(Form(a, "grad"))
+    asm_1 = assembler(u_space, None, Measure(1, (2,)), 3, ("f", "grad", "dx", "n"))
+    g = asm_1.linear(Form(bc, "f"))
+    u_sol = spsolve(A, f + g).reshape(-1, 1)
+    u_err = Function(u_space)
+    u_err[:] = u_sol
+    u_err -= asm_2.functional(Form(integral, "f"), u=u_err) / 1.0
+    #
+    fig = pyplot.figure()
+    ax_sol = fig.add_subplot(1, 2, 1, projection="3d")
+    ax_sol.plot_trisurf(mesh.point[:,0], mesh.point[:,1], u_err.ravel(), triangles=mesh.cell[2][:,:-1], cmap=pyplot.cm.Spectral)
+    #
+    u_exact = exact(mesh.point[:, 0], mesh.point[:, 1]).reshape(-1, 1)
+    u_err -= u_exact
+    u_err = u_err - asm_2.functional(Form(integral, "f"), u=u_err) / 1.0
+    #
+    ax_err = fig.add_subplot(1, 2, 2, projection="3d")
+    ax_err.plot_trisurf(mesh.point[:,0], mesh.point[:,1], u_err.ravel(), triangles=mesh.cell[2][:,:-1], cmap=pyplot.cm.Spectral)
+    # pyplot.show()
+    print("Max-norm of error = {0:.3e}".format(np.linalg.norm(u_err, ord=np.inf)))
