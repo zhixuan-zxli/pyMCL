a2f4a94ef7e3d4c5a996d12b3e713f97bb936d87
Zhixuan Li <zihinlai@163.com>
2024-03-10 17:38:53 +0800

    Reduce to np.array instead of Function for geometric data.

--- function.py
+++ function.py
@@ -66,14 +66,14 @@ class Function(np.ndarray):
             data = np.zeros((rdim, cell_dof.shape[0], quadTable.shape[1]))
             for i in range(num_dof_per_elem):
                 basis = basis_type._eval_basis(i, quadTable) # (rdim, Nq)
-                temp = self[cell_dof[:,i], :].T
+                temp = np.array(self)[cell_dof[:,i], :].T
                 data = data + temp[:,:,np.newaxis] * basis[:, np.newaxis, :] # (rdim, Ne, Nq)
         data = QuadData(data)
         if "grad" in hint:
-            grad = np.zeros((rdim, self.fe.tdim, cell_dof.shape[0], quadTable.shape[1])) # (rdim, tdim, Ne, Nq)
+            grad = np.zeros((rdim, tdim, cell_dof.shape[0], quadTable.shape[1])) # (rdim, tdim, Ne, Nq)
             for i in range(num_dof_per_elem):
                 basis_grad = basis_type._eval_grad(i, quadTable) # (rdim, tdim, Nq)
-                temp = self[cell_dof[:,i], :].T
+                temp = np.array(self)[cell_dof[:,i], :].T
                 grad = grad + temp[:,np.newaxis,:,np.newaxis] * basis_grad[:, :, np.newaxis, :]
             if x is not None:
                 grad = np.einsum("ij...,jk...->ik...", grad, x.inv_grad)
@@ -94,33 +94,33 @@ class Function(np.ndarray):
             assert x is None
             if tdim == 1:
                 assert(rdim == 2)
-                t = np.squeeze(grad) / data.dx[np.newaxis, :, :]
+                t = np.squeeze(grad) / data.dx
                 data.n = np.zeros_like(t)
-                data.n[0, :] = t[1, :]
-                data.n[1, :] = -t[0, :]
+                data.n[0] = t[1]
+                data.n[1] = -t[0]
             elif tdim == 2:
                 assert(rdim == 3)
                 temp = np.cross(grad[:, 0, :, :], grad[:, 1, :, :], axis=0) # (3, Ne, Nq)
-                data.n = temp / data.dx[np.newaxis, :, :]
+                data.n = temp / data.dx
             else:
                 raise RuntimeError("Cannot calculate the normal of a 3D cell. ")
         if "inv_grad" in hint:
             assert x is None
-            if self.fe.tdim == 1 and rdim == 1:
+            if tdim == 1 and rdim == 1:
                 data.inv_grad = 1.0 / data.grad
-            elif self.fe.tdim == 1 and rdim == 2:
+            elif tdim == 1 and rdim == 2:
                 temp = np.squeeze(data.grad) # (2, Ne, Nq)
-                data.inv_grad = (temp / data.dx[np.newaxis, :, :]**2).reshape(1, 2, data.grad.shape[2], data.grad.shape[3])
-            elif self.fe.tdim == 2 and rdim == 2:
+                data.inv_grad = (temp / data.dx**2).reshape(1, 2, data.grad.shape[2], data.grad.shape[3])
+            elif tdim == 2 and rdim == 2:
                 data.inv_grad = np.zeros_like(data.grad)
-                data.inv_grad[0, 0, :, :] = data.grad[1, 1, :, :] / data.dx
-                data.inv_grad[0, 1, :, :] = -data.grad[0, 1, :, :] / data.dx
-                data.inv_grad[1, 0, :, :] = -data.grad[1, 0, :, :] / data.dx
-                data.inv_grad[1, 1, :, :] = data.grad[0, 0, :, :] / data.dx
-            elif self.fe.tdim == 2 and rdim == 3:
+                data.inv_grad[0, 0, :, :] = data.grad[1, 1, :, :] / data.dx[0,:,:]
+                data.inv_grad[0, 1, :, :] = -data.grad[0, 1, :, :] / data.dx[0,:,:]
+                data.inv_grad[1, 0, :, :] = -data.grad[1, 0, :, :] / data.dx[0,:,:]
+                data.inv_grad[1, 1, :, :] = data.grad[0, 0, :, :] / data.dx[0,:,:]
+            elif tdim == 2 and rdim == 3:
                 data.inv_grad = np.zeros((2, 3, data.grad.shape[2], data.grad.shape[3]))
-                data.inv_grad[0, :, :, :] = np.cross(data.grad[:,1,:,:], data.n, axis=0) / data.dx[np.newaxis, :, :]
-                data.inv_grad[1, :, :, :] = np.cross(data.n, data.grad[:,0,:,:], axis=0) / data.dx[np.newaxis, :, :]
+                data.inv_grad[0, :, :, :] = np.cross(data.grad[:,1,:,:], data.n, axis=0) / data.dx
+                data.inv_grad[1, :, :, :] = np.cross(data.n, data.grad[:,0,:,:], axis=0) / data.dx
             else:
                 raise RuntimeError("Unable to calculate inv_grad for tdim={} and rdim={}.".format(self.fe.tdim, rdim))
         # todo: conormal
