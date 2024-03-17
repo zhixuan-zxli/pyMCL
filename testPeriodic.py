32b6c4b656fdedc15abdc1e77e1fa265dd564cc8
Zhixuan Li <zihinlai@163.com>
2024-03-17 13:49:54 +0800

    Tested periodic-Neumann Poisson with P1/P2 element.

--- testPeriodic.py
+++ testPeriodic.py
@@ -13,7 +13,11 @@ from math import sqrt
 def exact(x, y) -> np.ndarray:
     return np.sin(2.0*np.pi*x) * np.cos(np.pi*y)**2
 
-def rhs(psi, coord) -> np.ndarray:
+def exact_grad(x, y) -> np.ndarray:
+    return np.array((2.0*np.pi*np.cos(2.0*np.pi*x) * np.cos(np.pi*y)**2, 
+                     -np.pi*np.sin(2.0*np.pi*x) * np.sin(2.0*np.pi*y)))
+
+def f(psi, coord) -> np.ndarray:
     # psi: (1,1,Nq)
     # coord: (2,Ne,Nq)
     x, y = coord[0], coord[1]
@@ -26,6 +30,18 @@ def a(phi, psi, coord) -> np.ndarray:
     data = np.sum(phi.grad * psi.grad, axis=1)
     data = data * coord.dx
     return data[:,np.newaxis]
+
+def g(psi, coord) -> np.ndarray:
+    x, y = coord[0], coord[1] # (Ne, Nq)
+    ngrad = np.sum(exact_grad(x, y) * coord.n, axis=0, keepdims=True) # (1, Ne, Nq)
+    return ngrad * psi * coord.dx
+
+def test(psi, coord) -> np.ndarray:
+    # psi: (1, Ne, Nq)
+    return psi * coord.dx
+
+def integral(coord, u) -> np.ndarray:
+    return u * coord.dx
 
 def L2(coord, u) -> np.ndarray:
     return u**2 * coord.dx
@@ -44,52 +60,45 @@ if __name__ == "__main__":
             mesh.load("mesh/unit_square.msh")
         else:
             mesh = splitRefine(mesh)
-        # Affine mesh
-        setMeshMapping(mesh)
+        setMeshMapping(mesh) # affine mesh
         mesh.add_constraint(lambda x: np.abs(x[:,0]) < 1e-14, 
                             lambda x: np.abs(x[:,0]-1.0) < 1e-14, 
                             lambda x: x + np.array(((1.0, 0.0))), 
                             tol=1e-11)
-        # mesh.add_constraint(lambda x: np.abs(x[:,1]) < 1e-14, 
-        #                     lambda x: np.abs(x[:,1]-1.0) < 1e-14, 
-        #                     lambda x: x + np.array(((0.0, 1.0))), 
-        #                     tol=1e-11)
-        
-        # fe = TriP1(mesh, 1, periodic=False)
-        fe = TriP1(mesh, 1, periodic=True)
+
+        fe = TriP2(mesh, 1, periodic=True)
 
         asm_2 = assembler(fe, fe, Measure(2), order=3)
-        f = asm_2.linear(Form(rhs, "f"))
+        F = asm_2.linear(Form(f, "f"))
         A = asm_2.bilinear(Form(a, "grad"))
+        V = asm_2.linear(Form(test, "f"))
+        G = assembler(fe, None, Measure(1, (2,4)), 3).linear(Form(g, "f"))
+
+        # the augmented system
+        Aa = sparse.bmat(((A, V), (V.T, None)), format="csr")
+        Fa = np.vstack((F+G, 0.0))
 
         # get freeze dof
-        # dirichlet_dof = [0]
-        dirichlet_dof = np.unique(fe.getCellDof(Measure(1, (2, 4))))
-        # dirichlet_dof = np.unique(fe.getCellDof(Measure(1, (2, 3, 4, 5))))
         slave_dof = np.nonzero(fe.dof_remap != np.arange(fe.num_dof))[0]
-        freeze_dof = np.unique(np.concatenate((dirichlet_dof, slave_dof)))
-        free_dof = np.ones((fe.num_dof, ), dtype=np.bool8)
-        free_dof[freeze_dof] = False
-        # free_dof[dirichlet_dof] = False
+        free_dof = np.ones((fe.num_dof + 1, ), dtype=np.bool8)
+        free_dof[slave_dof] = False
 
-        u_e = Function(fe)
-        u_e[:,0] = exact(fe.dofloc[:,0], fe.dofloc[:,1])
+        # system the system
+        sol_vec = spsolve(Aa[free_dof][:,free_dof], Fa[free_dof])
         u = Function(fe)
-        u[dirichlet_dof] = u_e[dirichlet_dof]
-
-        # homogeneize the system
-        f = f - A @ u
-        sol_vec = spsolve(A[free_dof][:,free_dof], f[free_dof])
-        u[free_dof,0] = sol_vec
+        u[free_dof[:-1],0] = sol_vec[:-1]
         u.update()
 
-        u_e -= u
+        u_err = Function(fe)
+        u_err[:,0] = exact(fe.dofloc[:,0], fe.dofloc[:,1])
+        u_err -= u
+        u_err -= asm_2.functional(Form(integral, "f"), u=u_err) / 1.0
         
         # ax_err = fig.add_subplot(1, 2, 2, projection="3d")
         # ax_err.plot_trisurf(mesh.point[:,0], mesh.point[:,1], u_err.ravel(), triangles=mesh.cell[2][:,:-1], cmap=pyplot.cm.Spectral)
         # pyplot.show()
-        error_table["infty"][m] = np.linalg.norm(u_e, ord=np.inf)
-        error_table["L2"][m] = sqrt(asm_2.functional(Form(L2, "f"), u=u_e))
+        error_table["infty"][m] = np.linalg.norm(u_err, ord=np.inf)
+        error_table["L2"][m] = sqrt(asm_2.functional(Form(L2, "f"), u=u_err))
 
     printConvergenceTable(mesh_table, error_table)
     
