5c5bff16a88c141f22b817bbf35fbd4d47105e50
Zhixuan Li <zihinlai@163.com>
2024-04-02 12:23:44 +0800

    Add MeshMapping._interpolate_cell.

--- fem/function.py
+++ fem/function.py
@@ -3,27 +3,36 @@ import numpy as np
 from .mesh import Mesh
 from .fe import FiniteElement
 
-class FieldData(np.ndarray):
+class QuadData(np.ndarray):
     """
     Discrete function values on quadrature points, (rdim * Ne * Nquad).
     """
+
+    grad: np.ndarray
+    inv_grad: np.ndarray
+    dx: np.ndarray
+    cn: np.ndarray # cell normal
+    fn: np.ndarray # facet normal
+
     def __new__(cls, value: Optional[np.ndarray] = None):
         if value is None:
             obj = np.array((0,)).view(cls)
         else:
             obj = value.view(cls)
         obj.grad = None
-        obj.dx = None
-        obj.n = None
         obj.inv_grad = None
+        obj.dx = None
+        obj.cn = None
+        obj.fn = None
         return obj
     
     def __array_finalize__(self, obj) -> None:
         if obj is None: return
         self.grad = getattr(obj, "grad", None)
-        self.dx = getattr(obj, "dx", None)
-        self.n = getattr(obj, "n", None)
         self.inv_grad = getattr(obj, "inv_grad", None)
+        self.dx = getattr(obj, "dx", None)
+        self.cn = getattr(obj, "cn", None)
+        self.fn = getattr(obj, "fn", None)
 
     def __array_wrap__(self, out_arr, context=None):
         # invalidate the attributes
@@ -108,7 +117,7 @@ class Function(np.ndarray):
         if self.fe.periodic:
             raise NotImplementedError
     
-    def _interpolate_cell(self, mea: CellMeasure, quad_tab: np.ndarray, x: Optional[FieldData] = None) -> FieldData:
+    def _interpolate_cell(self, mea: CellMeasure, quad_tab: np.ndarray, x: Optional[QuadData] = None) -> QuadData:
         tdim, rdim = self.fe.elem.tdim, self.fe.elem.rdim
         elem_dof = self.fe.elem_dof[:, mea.elem_ix]
         Ne = elem_dof.shape[1]
@@ -119,21 +128,20 @@ class Function(np.ndarray):
             temp = self.view(np.ndarray)[elem_dof[i]] # (Ne, )
             # interpolate function values
             basis_data = self.fe.elem._eval_basis(i, quad_tab) # (rdim, Nq)
-            data += temp[np.newaxis] * basis_data[:, np.newaxis] # (rdim, Ne, Nq)
+            data += temp[np.newaxis,:,np.newaxis] * basis_data[:, np.newaxis] # (rdim, Ne, Nq)
             # interpolate the gradients
             grad_data = self.fe.elem._eval_grad(i, quad_tab) # (rdim, elem.tdim, Nq)
-            grad_temp = temp[np.newaxis, np.newaxis] * grad_data[:,:,np.newaxis] # (rdim, elem.tdim, Ne, Nq)
+            grad_temp = temp[np.newaxis,np.newaxis,:,np.newaxis] \
+                * grad_data[:,:,np.newaxis] # (rdim, elem.tdim, Ne, Nq)
             if x is not None:
                 grad_temp = np.einsum("ij...,jk...->ik...", grad_temp, x.inv_grad)
             grad += grad_temp
-            # interpolate other things ...
-            if x is None:
-                pass
-        data = FieldData(data)
+            
+        data = QuadData(data)
         data.grad = grad
         return data
     
-    def _interpolate_facet(self, mea: FaceMeasure, quad_tab: np.ndarray, x: Optional[FieldData] = None) -> FieldData:
+    def _interpolate_facet(self, mea: FaceMeasure, quad_tab: np.ndarray, x: Optional[QuadData] = None) -> QuadData:
         tdim, rdim = self.fe.elem.tdim, self.fe.elem.rdim
         Nq = quad_tab.shape[1]
         # transform the quadrature locations via facet_id here
@@ -159,6 +167,53 @@ class Function(np.ndarray):
                     pass
             res.append(data)
         return res if len(res) > 1 else res[0]
+    
+class MeshMapping(Function):
+
+    def __new__(cls, fe: FiniteElement):
+        obj = np.zeros((fe.num_dof,)).view(cls)
+        obj.fe = fe
+        return obj
+    
+    def _interpolate_cell(self, mea: CellMeasure, quad_tab: np.ndarray) -> QuadData:
+        data = super()._interpolate_cell(mea, quad_tab, None) 
+        tdim, rdim = self.fe.elem.tdim, self.fe.elem.rdim
+        assert tdim == rdim or tdim == rdim-1
+        Ne, Nq = data.shape[1:]
+        # grad: (rdim, tdim, Ne, Nq)
+        # build dx: (1, Ne, Nq)
+        # build cn: (rdim, Ne, Nq)
+        # build inv_grad: (tdim, rdim, Ne, Nq)
+        if tdim == 0:
+            data.dx = np.ones((1, Ne, Nq))
+            data.cn = np.ones((rdim, Ne, Nq)) if rdim > tdim else None
+            data.inv_grad = np.zeros((0, rdim, Ne, Nq))
+        elif tdim == 1:
+            data.dx = np.linalg.norm(data.grad, ord=None, axis=0)
+            if rdim == 1:
+                data.inv_grad = 1.0 / data.grad
+            elif rdim == 2:
+                t = data.grad[:,0] / data.dx # (2, Ne, Nq)
+                data.cn = np.array((t[1], -t[0]))
+                data.inv_grad = data.grad[:,0] / data.dx**2
+                data.inv_grad = data.inv_grad[np.newaxis]
+        elif tdim == 2:
+            data.dx = np.cross(data.grad[:,0], data.grad[:,1], axis=0) # (Ne,Nq) or (3,Ne,Nq)
+            if rdim == 3:
+                data.cn = data.dx
+                data.dx = np.linalg.norm(data.dx, ord=None, axis=0) #(Ne, Nq)
+                data.cn = data.cn / data[np.newaxis]
+            data.dx = data.dx[np.newaxis]
+            data.inv_grad = np.zeros((tdim, rdim, Ne, Nq))
+            if rdim == 2:
+                data.inv_grad[0,0] = data.grad[1,1] / data.dx[0]
+                data.inv_grad[0,1] = -data.grad[0,1] / data.dx[0]
+                data.inv_grad[1,0] = -data.grad[1,0] / data.dx[0]
+                data.inv_grad[1,1] = data.grad[0,0] / data.dx[0]
+            elif rdim == 3:
+                data.inv_grad[0] = np.cross(data.grad[:,1], data.cn, axis=0) / data.dx
+                data.inv_grad[1] = np.cross(data.cn, data.grad[:,0], axis=0) / data.dx
+        
 
 
 
--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-from fem.mesh import Mesh
+from fem.mesh import *
 from fem.element import *
 from fem.fe import *
+from fem.function import *
+from fem.quadrature import *
 from matplotlib import pyplot
 
 if __name__ == "__main__":
     mesh = Mesh()
-    mesh.load("mesh/two-phase.msh")
+    mesh.load("mesh/unit_square.msh")
+    space = FiniteElement(mesh, TriP1)
+    mea = CellMeasure(mesh)
+    u = MeshMapping(space)
+    q = Quadrature.getTable(RefTri, 3)
+    u._interpolate_cell(mea, q)
+    pass
     
