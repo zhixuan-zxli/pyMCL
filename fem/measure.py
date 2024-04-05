1e9b4dce7a8999415ddb6f7b29903d774b58f988
Zhixuan Li <zihinlai@163.com>
2024-04-05 17:09:26 +0800

    Combine volume/surface measure into one.

--- fem/measure.py
+++ fem/measure.py
@@ -6,54 +6,47 @@ class Measure:
     
     mesh: Mesh
     elem_ix: Union[np.ndarray, slice] # the element indices involved
+    facet_ix: tuple[np.ndarray] # the facet indices of a surface measure
+    facet_id: tuple[np.ndarray] # the facet if within an element, for a surface measure
 
-
-class CellMeasure(Measure):
-    """
-    Represent the volume measure whose dimension equals the topological dimension of the mesh. 
-    """
-
-    # mesh: Mesh
-    # elem_ix: Union[np.ndarray, slice]
-
-    def __init__(self, mesh: Mesh, tags: Optional[tuple[int]] = None) -> None:
+    def __init__(self, mesh: Mesh, 
+                 dim: int, 
+                 tags: Optional[tuple[int]] = None, 
+                 interiorFacet: bool = False) -> None:
+        """
+        If dim == mesh.tdim, represent the volume measure. 
+        If dim == mesh.tdim-1, represent the surface measure. 
+        """
         self.mesh = mesh
-        if tags is None:
-            self.elem_ix = slice(None) # select all the elements
+        # 1. Volume measure
+        if dim == mesh.tdim:
+            if tags is None:
+                self.elem_ix = slice(None) # select all the elements
+            else:
+                # select the elements with the provided tags
+                elem_tag = mesh.cell_tag[mesh.tdim]
+                flag = np.zeros((elem_tag.shape[0],), dtype=np.bool8)
+                for t in tags:
+                    flag[elem_tag == t] = True
+                self.elem_ix = np.nonzero(flag)[0]
+        # 2. Surface measure
+        elif dim == mesh.tdim-1:
+            if tags is None:
+                self.facet_ix = slice(None)
+            else:
+                # select the facets with the provided tags
+                facet_tag = mesh.cell_tag[mesh.tdim-1]
+                flag = np.zeros((facet_tag.shape[0],), dtype=np.bool8)
+                for t in tags:
+                    flag[facet_tag == t] = True
+                self.facet_ix = np.nonzero(flag)[0]
+            #
+            elem_ix = []
+            facet_id = []
+            for k in range(1+interiorFacet):
+                elem_ix.append(mesh.facet_ref[k,0,self.facet_ix])
+                facet_id.append(mesh.facet_ref[k,1,self.facet_ix])
+            self.elem_ix = tuple(elem_ix)
+            self.facet_id = tuple(facet_id)
         else:
-            # select the elements with the provided tags
-            elem_tag = mesh.cell_tag[mesh.tdim]
-            flag = np.zeros((elem_tag.shape[0],), dtype=np.bool8)
-            for t in tags:
-                flag[elem_tag == t] = True
-            self.elem_ix = np.nonzero(flag)[0]
-
-class FaceMeasure(Measure):
-    """
-    Represent the surface measure whose dimension is one less than the topological dimension of the mesh. 
-    """
-    
-    # mesh: Mesh
-    facet_ix: tuple[np.ndarray]
-    # elem_ix: tuple[np.ndarray]
-    facet_id: tuple[np.ndarray]
-
-    def __init__(self, mesh: Mesh, tags: Optional[tuple[int]] = None, interiorFacet: bool = False) -> None:
-        self.mesh = mesh
-        if tags is None:
-            self.facet_ix = slice(None)
-        else:
-            # select the facets with the provided tags
-            facet_tag = mesh.cell_tag[mesh.tdim-1]
-            flag = np.zeros((facet_tag.shape[0],), dtype=np.bool8)
-            for t in tags:
-                flag[facet_tag == t] = True
-            self.facet_ix = np.nonzero(flag)[0]
-        #
-        elem_ix = []
-        facet_id = []
-        for k in range(1+interiorFacet):
-            elem_ix.append(mesh.facet_ref[k,0,self.facet_ix])
-            facet_id.append(mesh.facet_ref[k,1,self.facet_ix])
-        self.elem_ix = tuple(elem_ix)
-        self.facet_id = tuple(facet_id)
+            raise RuntimeError("This measure is neither a volume measure nor a surface measure.")
