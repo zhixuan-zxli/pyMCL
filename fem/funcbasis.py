50abadd3b38495888604aa25963ceaa2a3830336
Zhixuan Li <zihinlai@163.com>
2024-04-07 15:56:30 +0800

    Add FunctionBasis.update.

--- fem/funcbasis.py
+++ fem/funcbasis.py
@@ -13,31 +13,36 @@ class FunctionBasis:
     mea: Measure
     data: tuple[Union[QuadData, tuple[QuadData]]] # size = number of local dofs
 
-    def __init__(self, fs: FunctionSpace, mea: Measure, quad_tab: np.ndarray, x: QuadData) -> None:
+
+    def __init__(self, fs: FunctionSpace, mea: Measure) -> None:
         self.fs = fs
         self.mea = mea
-        tdim, rdim = fs.elem.tdim, fs.elem.rdim
-        num_local_dof = fs.elem.num_local_dof
-        assert mea.mesh is fs.mesh
+        self.update()
+
+    def update(self) -> None:
+        tdim, rdim = self.fs.elem.tdim, self.fs.elem.rdim
+        num_local_dof = self.fs.elem.num_local_dof
+        assert self.mea.mesh is self.fs.mesh
+        quad_tab = self.mea.quad_tab
         Nq = quad_tab.shape[1]
         self.data = []
         #
-        if mea.dim == mea.mesh.tdim:
+        if self.mea.dim == tdim:
             for i in range(num_local_dof):
-                u, du = fs.elem._eval(i, quad_tab)
+                u, du = self.fs.elem._eval(i, quad_tab)
                 # u: (rdim, Nq)
                 # du: (rdim, tdim, Nq)
                 data = QuadData(u.reshape(rdim, 1, Nq)) # (rdim, 1, Nq)
                 data.grad = np.einsum("ij...,jk...->ik...", \
-                                      du[:,:,np.newaxis,:], x.inv_grad) # (rdim, tdim, Ne, Nq)
+                                      du[:,:,np.newaxis,:], self.mea.x.inv_grad) # (rdim, tdim, Ne, Nq)
                 self.data.append(data)
         #
-        if mea.dim == mea.mesh.tdim-1:
-            quad_tab = fs.elem.ref_cell._broadcast_facet(quad_tab) # (tdim, num_facet, Nq)
+        elif self.mea.dim == tdim-1:
+            quad_tab = self.fs.elem.ref_cell._broadcast_facet(quad_tab) # (tdim, num_facet, Nq)
             for i in range(num_local_dof):
                 data_tuple = []
-                for facet_id, y in zip(mea.facet_id, x):
-                    u, du = fs.elem._eval(i, quad_tab.reshape(tdim, -1))
+                for y, facet_id in zip(self.mea.x, self.mea.facet_id):
+                    u, du = self.fs.elem._eval(i, quad_tab.reshape(tdim, -1))
                     # u: (rdim, num_facet * Nq)
                     # du: (rdim, tdim, num_facet* Nq)
                     # facet_id: (Ne, )
@@ -48,5 +53,8 @@ class FunctionBasis:
                     data_tuple.append(data)
                 self.data.append(tuple(data_tuple))
         #
+        else:
+            raise RuntimeError("Incorrect measure dimension. ")
+        # transform the data into a tuple
         self.data = tuple(self.data)
 
