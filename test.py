c33d028a3b14d5ccec8a4ababe1cf42ac4cf5ee2
Zhixuan Li <zihinlai@163.com>
2024-04-08 20:49:14 +0800

    TriP1, TriP2, their vector elements and their grad tested.

--- test.py
+++ test.py
@@ -13,16 +13,15 @@ def L2(x: QuadData, u: QuadData) -> np.ndarray:
     # x.dx: (1, Ne, Nq)
     # u: (1, Ne, Nq)
     # u.grad: (1, 2, Ne, Nq)
-    # return u**2 * x.dx
-    return u[0][np.newaxis]**2 * x.dx
+    return u**2 * x.dx
+    # return x.dx
 
-
-# @Form
-# def L2s(x: tuple[QuadData], u: QuadData) -> np.ndarray:
-#     x, u = x[0], u[0]
-#     # u: (1, Ne, Nq)
-#     # x.ds: (1, Ne, Nq)
-#     return u**2 * x.ds
+@Form
+def L2s(x: tuple[QuadData], u: QuadData) -> np.ndarray:
+    x, u = x[0], u[0]
+    # u: (1, Ne, Nq)
+    # x.ds: (1, Ne, Nq)
+    return u**2 * x.ds
 
 def u_exact(x, y) -> np.ndarray:
     return np.sin(np.pi*x) * np.cos(y)
@@ -30,20 +29,19 @@ def u_exact(x, y) -> np.ndarray:
 if __name__ == "__main__":
     mesh = Mesh()
     mesh.load("mesh/unit_square.msh")
-    setMeshMapping(mesh)
+    setMeshMapping(mesh, 2)
 
-    P2 = FunctionSpace(mesh, VectorElement(TriP2, 2))
-    u = Function(P2)
-    dof_0 = P2.dof_group["u_0"]
-    u[dof_0] = u_exact(P2.dof_loc[dof_0,0], P2.dof_loc[dof_0,1])
+    fs = FunctionSpace(mesh, TriP2)
+    u = Function(fs)
+    u[:] = u_exact(fs.dof_loc[:,0], fs.dof_loc[:,1])
 
     dx = Measure(mesh, 2, order=3)
     u_ = u._interpolate(dx)
     print("L2 integral = {}".format(L2.functional(dx, u=u_)))
     print("exact = {}".format(0.5 * (0.5 + 0.25*np.sin(2.))))
 
-    # ds = Measure(mesh, 1, order=3, tags=(4,))
-    # u_ = u._interpolate(ds)
-    # print("L2 surface integral / cos(1.)^2 = {}".format(L2s.functional(ds, u=u_) / np.cos(1.0)**2))
+    ds = Measure(mesh, 1, order=3, tags=(4,))
+    u_ = u._interpolate(ds)
+    print("L2 surface integral / cos(1.)^2 = {}".format(L2s.functional(ds, u=u_) / np.cos(1.0)**2))
 
     
