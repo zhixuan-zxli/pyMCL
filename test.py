5dc46c0bc8bd1c35e2e9b1f468589cc608cb552b
Zhixuan Li <zihinlai@163.com>
2024-04-08 21:20:48 +0800

    Mixed dim looks OK.

--- test.py
+++ test.py
@@ -8,20 +8,14 @@ from fem.form import *
 from matplotlib import pyplot
 
 @Form
-def L2(x: QuadData, u: QuadData) -> np.ndarray:
+def L2(x: QuadData, u: QuadData, v: QuadData) -> np.ndarray:
     # x: (gdim, Ne, Nq)
     # x.dx: (1, Ne, Nq)
     # u: (1, Ne, Nq)
     # u.grad: (1, 2, Ne, Nq)
-    return u**2 * x.dx
-    # return x.dx
-
-@Form
-def L2s(x: tuple[QuadData], u: QuadData) -> np.ndarray:
-    x, u = x[0], u[0]
-    # u: (1, Ne, Nq)
-    # x.ds: (1, Ne, Nq)
-    return u**2 * x.ds
+    x = x[0]
+    u = u[0]
+    return u * v * x.ds
 
 def u_exact(x, y) -> np.ndarray:
     return np.sin(np.pi*x) * np.cos(y)
@@ -29,19 +23,23 @@ def u_exact(x, y) -> np.ndarray:
 if __name__ == "__main__":
     mesh = Mesh()
     mesh.load("mesh/unit_square.msh")
-    setMeshMapping(mesh, 2)
+    setMeshMapping(mesh)
 
-    fs = FunctionSpace(mesh, TriP2)
-    u = Function(fs)
-    u[:] = u_exact(fs.dof_loc[:,0], fs.dof_loc[:,1])
+    b_mesh = mesh.view(1, (4,))
+    setMeshMapping(b_mesh)
 
-    dx = Measure(mesh, 2, order=3)
-    u_ = u._interpolate(dx)
-    print("L2 integral = {}".format(L2.functional(dx, u=u_)))
-    print("exact = {}".format(0.5 * (0.5 + 0.25*np.sin(2.))))
+    fs2 = FunctionSpace(mesh, TriP2)
+    u = Function(fs2)
+    u[:] = u_exact(fs2.dof_loc[:,0], fs2.dof_loc[:,1])
+
+    fs1 = FunctionSpace(b_mesh, LineP2)
+    v = Function(fs1)
+    v[:] = u_exact(fs1.dof_loc[:,0], fs1.dof_loc[:,1])
 
     ds = Measure(mesh, 1, order=3, tags=(4,))
-    u_ = u._interpolate(ds)
-    print("L2 surface integral / cos(1.)^2 = {}".format(L2s.functional(ds, u=u_) / np.cos(1.0)**2))
+    dx = Measure(b_mesh, 1, order=3)
+
+    r = L2.functional(ds, u=u._interpolate(ds), v=v._interpolate(dx))
+    print("L2 surface integral / cos(1.)^2 = {}".format(r / np.cos(1.0)**2))
 
     
