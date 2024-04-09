9d203124f9b4412ea7e8b1d37c83eeb165d85b1e
Zhixuan Li <zihinlai@163.com>
2024-04-09 21:50:46 +0800

    Recover group_dof.

--- fem/funcspace.py
+++ fem/funcspace.py
@@ -119,28 +119,21 @@ class FunctionSpace:
             for t in tags:
                 flag[facet_tag == t] = True
         return self.facet_dof[:, flag]
-    
-# def group_dof(mixed_fe: tuple[Element], dof_list) -> np.ndarray:
-#     """
-#     Get the free dof (as a bool mask) for a mixed finite element space. 
-#     mixed_fe: a tuple of Element objects. 
-#     dof_list: a tuple of dof lists. dof_list[c] is the fixed dof for mixed_fe[c]. 
-#     It can be 
-#     (1) None, or a tuple of array of dofs to be fixed if mixed_fe[c] has a single copy, 
-#     (2) or a tuple of (1) for each copy of the finite element space. 
-#     """
-#     total_num_dof = sum(fe.num_dof * fe.num_copy for fe in mixed_fe)
-#     free_dof = np.ones((total_num_dof, ), dtype=np.bool8)
-#     # combine these dof to get the free dof
-#     base_index = 0
-#     for fe, dof in zip(mixed_fe, dof_list):
-#         if fe.num_copy == 1:
-#             dof = (dof, )
-#         for c in range(fe.num_copy):
-#             if dof[c] is None:
-#                 continue
-#             assert isinstance(dof[c], tuple)
-#             for dd in dof[c]:
-#                 free_dof[dd.reshape(-1)*fe.num_copy+c+base_index] = False
-#         base_index += fe.num_dof * fe.num_copy
-#     return free_dof
+
+
+def group_dof(mixed_fe: tuple[FunctionSpace], dof_list: tuple[Optional[np.ndarray]]) -> np.ndarray:
+    """
+    Get the free dof (as a bool mask) for a mixed finite element space. 
+    mixed_fe: a tuple of Element objects. 
+    dof_list: a tuple of dof lists. dof_list[c] is either or the fixed dof for mixed_fe[c].
+    """
+    total_num_dof = sum(fe.num_dof for fe in mixed_fe)
+    free_dof = np.ones((total_num_dof, ), dtype=np.bool8)
+    # combine these dof to get the free dof
+    offset = 0
+    for fs, dof in zip(mixed_fe, dof_list):
+        if dof is None:
+            continue
+        free_dof[offset+dof] = False
+        offset += fs.num_dof
+    return free_dof
