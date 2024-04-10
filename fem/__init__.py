a5c01b9e3cb14c7d586202426120712fa99415ac
Zhixuan Li <zihinlai@163.com>
2024-04-10 19:24:12 +0800

    Add __init__.

---
+++ fem/__init__.py
@@ -0,0 +1,9 @@
+
+from fem.mesh import *
+from fem.mesh_util import *
+from fem.element import *
+from fem.funcspace import *
+from fem.function import *
+from fem.funcbasis import *
+from fem.form import *
+from fem.post import *
