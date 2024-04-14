27f62722b340f74ec1be041366a86e2fc7bb9e86
Zhixuan Li <zihinlai@163.com>
2024-04-14 13:21:32 +0800

    Weak imposition of essential BC using Nitsche method looks good; DG is still not working.

--- testPoisson.py
+++ testPoisson.py
@@ -46,15 +46,37 @@ def integral(x, u) -> np.ndarray:
 def L2(x, u) -> np.ndarray:
     return u**2 * x.dx
 
+gamma = 20.0 # stablization parameter for DG
+
+@BilinearForm
+def a_DG(v: QuadData, u: QuadData, xv: QuadData, xu: QuadData) -> np.ndarray:
+    return -0.5 * v * np.sum(u.grad[0] * xv.fn, axis=0, keepdims=True) * xv.ds \
+    -0.5 * u * np.sum(v.grad[0] * xu.fn, axis=0, keepdims=True) * xu.ds \
+    + gamma * u * v * np.sum(xv.fn * xu.fn, axis=0, keepdims=True)
+
+@BilinearForm
+def a_nitsche(v: QuadData, u: QuadData, x: QuadData) -> np.ndarray:
+    return -v * np.sum(u.grad[0] * x.fn, axis=0, keepdims=True) * x.ds \
+    -u * np.sum(v.grad[0] * x.fn, axis=0, keepdims=True) * x.ds \
+    + gamma * u * v
+
+@LinearForm
+def l_nitsche(v: QuadData, x: QuadData) -> np.ndarray:
+    x1, x2 = x
+    g = u_exact(x1, x2)[np.newaxis]
+    return -g * np.sum(v.grad[0] * x.fn, axis=0, keepdims=True) * x.ds \
+    + gamma * g * v
+
 
 if __name__ == "__main__":
 
-    test_element = TriP2
+    test_element = TriP1
     num_hier = 3
     mesh_table = tuple(f"{i}" for i in range(num_hier))
     error_tab_D = {"infty" : [0.0] * num_hier, "L2" : [0.0] * num_hier}
     error_tab_N = {"infty" : [0.0] * num_hier, "L2" : [0.0] * num_hier}
     error_tab_P = {"infty" : [0.0] * num_hier, "L2" : [0.0] * num_hier}
+    error_tab_DG = {"infty" : [0.0] * num_hier, "L2" : [0.0] * num_hier}
     mesh = Mesh()
     mesh.load("mesh/unit_square.msh")
 
@@ -69,6 +91,8 @@ if __name__ == "__main__":
             mesh = splitRefine(mesh)
         setMeshMapping(mesh)
 
+        # ==================================================
+        # 1. test Dirichlet condition
         fs = FunctionSpace(mesh, test_element)
         dx = Measure(mesh, 2, order=3)
         u_basis = FunctionBasis(fs, dx)
@@ -76,8 +100,6 @@ if __name__ == "__main__":
         A = a.assemble(u_basis, u_basis, dx)
         L = l.assemble(u_basis, dx)
 
-        # ==================================================
-        # 1. test Dirichlet condition
         # impose the boundary condition
         bdof = np.unique(fs.getFacetDof((2, 3, 4, 5)))
         free_dof = group_dof((fs,), (bdof,))
@@ -143,7 +165,37 @@ if __name__ == "__main__":
         u_err = u - u_err
         error_tab_P["infty"][m] = np.linalg.norm(u_err, ord=np.inf)
         error_tab_P["L2"][m] = np.sqrt(L2.assemble(dx, u=u_err._interpolate(dx)))
-        print(".")
+        print(".", end="")
+
+        # ==================================================
+        # 4. test DG
+        # fs = FunctionSpace(mesh, TriDG1)
+        fs = FunctionSpace(mesh, test_element)
+        # dx remains unchanged
+        # dS = Measure(mesh, 1, order=3, tags=(INTERIOR_FACET_TAG, ), interiorFacet=True)
+        # ds remains unchanged
+        u_basis = FunctionBasis(fs, dx)
+        # u_i_basis = FunctionBasis(fs, dS)
+        u_s_basis = FunctionBasis(fs, ds)
+
+        A = a.assemble(u_basis, u_basis, dx)
+        # A_DG = a_DG.assemble(u_i_basis, u_i_basis, dS)
+        A_n = a_nitsche.assemble(u_s_basis, u_s_basis, ds)
+        L = l.assemble(u_basis, dx)
+        L_n = l_nitsche.assemble(u_s_basis, ds)
+
+        # solve the linear system
+        u = Function(fs)
+        u[:] = spsolve(A + A_n, L + L_n)
+        # u[:] = spsolve(A + A_DG + A_n, L + L_n)
+
+        # calculate the error
+        u_err = Function(fs)
+        u_err[:] = u_exact(fs.dof_loc[:,0], fs.dof_loc[:,1])
+        u_err = u - u_err
+        error_tab_DG["infty"][m] = np.linalg.norm(u_err, ord=np.inf)
+        error_tab_DG["L2"][m] = np.sqrt(L2.assemble(dx, u=u_err._interpolate(dx)))
+        print('.')
 
     print(Fore.GREEN + "Dirichlet problem: " + Style.RESET_ALL)
     printConvergenceTable(mesh_table, error_tab_D)
@@ -151,4 +203,6 @@ if __name__ == "__main__":
     printConvergenceTable(mesh_table, error_tab_N)
     print(Fore.GREEN + "\nPeriodic problem: " + Style.RESET_ALL)
     printConvergenceTable(mesh_table, error_tab_P)
+    print(Fore.GREEN + "Discontinuous Galerkin: " + Style.RESET_ALL)
+    printConvergenceTable(mesh_table, error_tab_DG)
     
