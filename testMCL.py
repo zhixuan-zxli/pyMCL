94f803c418b6d09d8018b5b442f85385486e81de
Zhixuan Li <zihinlai@163.com>
2024-04-16 19:05:39 +0800

    Preparing the forms.

--- testMCL.py
+++ testMCL.py
@@ -22,21 +22,87 @@ class SolverParemeters:
     numChekpoint: int = 0
     vis: bool = True
 
-@BilinearForm
-def c_lap(w: QuadData, q: QuadData, x: QuadData) -> np.ndarray:
-    return w.grad[0] * q.grad[0] * x.dx
+# ===========================================================
+# bilinear forms for the elastic sheet
 
 @BilinearForm
-def c_H0(w: QuadData, q: QuadData, x: QuadData) -> np.ndarray:
-    return w * q * x.dx
-
-@BilinearForm
-def c_cl(w: QuadData, q: QuadData, _, xq: QuadData) -> np.ndarray:
-    return w * (q.grad[0] * xq.fn) * xq.ds
+def c_L2(w: QuadData, q: QuadData, x: QuadData) -> np.ndarray:
+    return np.sum(w * q, axis=0, keepdims=True) * x.dx
 
 @LinearForm
-def l_ver(phi: QuadData, x: QuadData) -> np.ndarray:
-    return 0.5 * phi * x.ds
+def l_dq(w: QuadData, x: QuadData, q_k: QuadData) -> np.ndarray:
+    return np.sum(w * q_k.grad[:,0], axis=0, keepdims=True) * x.dx
+
+@BilinearForm
+def c_phitau(phi: QuadData, tau: QuadData, x: QuadData) -> np.ndarray:
+    # phi, tau: (2, Nf, Nq)
+    return np.sum(phi * tau, axis=0, keepdims=True) * x.dx
+
+@BilinearForm
+def c_phim3(phi: QuadData, m3: QuadData, x: QuadData) -> np.ndarray:
+    return np.sum(phi * m3, axis=0, keepdims=True) * x.ds
+
+@BilinearForm
+def c_cl_phim(phi: QuadData, m: QuadData, _, xm: QuadData) -> np.ndarray:
+    # phi: (2, Nf, Nq)
+    # m.grad: (1, 2, Nf, Nq)
+    # xm.fn: (2, Nf, Nq)
+    return (phi[1] * m.grad[0,0] * xm.fn[0])[np.newaxis] * xm.ds
+
+@BilinearForm
+def c_phim(phi: QuadData, m: QuadData, x: QuadData) -> np.ndarray:
+    # phi.grad: (2, 2, Nf, Nq)
+    # m.grad: (1, 2, Nf, Nq)
+    return (phi.grad[1,0] * m.grad[0,0])[np.newaxis] * x.dx
+
+@BilinearForm
+def c_phiq(phi: QuadData, q: QuadData, x: QuadData, q_k: QuadData) -> np.ndarray:
+    return 0.0
+
+
+# ===========================================================
+# bilinear forms for the fluid and fluid interface
+
+@BilinearForm
+def a_xiu(xi: QuadData, u: QuadData, x: QuadData, eta) -> np.ndarray:
+    # eta: (Ne,)
+    # grad: (2, 2, Ne, Nq)    
+    z = np.zeros(x.shape[1:]) # (Ne, Nq)
+    for i, j in (0,0), (0,1), (1,0), (1,1):
+        z += (u.grad[i,j] + u.grad[j,i]) * xi.grad[i,j]
+    return (z * eta[:, np.newaxis])[np.newaxis] * x.dx
+
+@BilinearForm
+def a_xip(xi: QuadData, p: QuadData, x: QuadData) -> np.ndarray:
+    # w.grad: (2,2,Ne,Nq)
+    # p: (1, 1, Nq)
+    z = (xi.grad[0,0] + xi.grad[1,1]) * p[0]
+    return z[np.newaxis] * x.dx
+
+@BilinearForm
+def a_xik(xi: QuadData, kappa: QuadData, x: QuadData) -> np.ndarray:
+    # kappa: (1, 1, Nq)
+    # w: (2, 1, Nq)
+    # x.fn: (2, Nf, Nq)
+    return np.sum(x.fn * xi, axis=0, keepdims=True) * kappa * x.ds # (2, Nf, Nq)
+
+@BilinearForm
+def a_xitau(xi: QuadData, tau: QuadData, x: QuadData) -> np.ndarray:
+    # xi: (2, Nf, Nq)
+    # tau: (2, Nf, Nq)
+    return np.sum(xi * tau, axis=0, keepdims=True) * x.ds
+
+@BilinearForm
+def a_zy(z: QuadData, y: QuadData, x: QuadData) -> np.ndarray:
+    return np.sum(z.grad, y.grad, axis=(0,1))[np.newaxis] * x.dx
+
+@BilinearForm
+def a_zk(z: QuadData, k: QuadData, x: QuadData) -> np.ndarray:
+    # k: (1, Nf, Nq)
+    return np.sum(z * x.cn, axis=0, keepdims=True) * k * x.dx 
+
+
+# ===========================================================
 
 
 if __name__ == "__main__":
@@ -49,9 +115,9 @@ if __name__ == "__main__":
     #              "right": 6, "top": 7, "left": 8, "cl": 9, "clamp": 10}
     mesh = Mesh()
     mesh.load("mesh/two-phase.msh")
-    # setMeshMapping(mesh, 2)
-    # i_mesh = mesh.view(1, tags=(3, )) # interface mesh
-    # setMeshMapping(i_mesh)
+    setMeshMapping(mesh, 2)
+    i_mesh = mesh.view(1, tags=(3, )) # interface mesh
+    setMeshMapping(i_mesh)
     s_mesh = mesh.view(1, tags=(4,5)) # sheet reference mesh
     setMeshMapping(s_mesh)
     # cl_mesh = mesh.view(0, sub_ids=(9, )) # contact line mesh
@@ -80,18 +146,12 @@ if __name__ == "__main__":
     # =================================================================
     # Step 1. Update the reference contact line. 
     # project the discontinuous deformation gradient onto P1 to find the conormal vector m1
-    @BilinearForm
-    def c_H0(w: QuadData, q: QuadData, x: QuadData) -> np.ndarray:
-        return np.sum(w * q, axis=0, keepdims=True) * x.dx
-    @LinearForm
-    def l_dq(w: QuadData, x: QuadData, q_k: QuadData) -> np.ndarray:
-        return np.sum(w * q_k.grad[:,0], axis=0, keepdims=True) * x.dx
 
-    C_H0 = c_H0.assemble(sheet_grad_basis, sheet_grad_basis, ds)
+    C_L2 = c_L2.assemble(sheet_grad_basis, sheet_grad_basis, ds)
     L_DQ = l_dq.assemble(sheet_grad_basis, ds, q_k = q_k._interpolate(ds))
 
     dq_k = Function(sheet_grad_space)
-    dq_k[:] = spsolve(C_H0, L_DQ)
+    dq_k[:] = spsolve(C_L2, L_DQ)
 
     # extract the conormal at the contact line
     dq_k_at_cl = dq_k.view(np.ndarray)[cl_dof_in_grad].reshape(-1, 2) # (-1, 2)
@@ -115,9 +175,7 @@ if __name__ == "__main__":
     s_mesh_vel = s_mesh_disp / solp.dt
     s_mesh.coord_map[::2] += s_mesh_disp
 
-    # q_k_ = q_k.view(np.ndarray)
-    # pyplot.plot(q_k[::2], q_k[1::2], 'ro')
-    # pyplot.quiver(q_k_[cl_dof_from_def[::2]], q_k_[cl_dof_from_def[1::2]], temp[:,0], temp[:,1])
-    # pyplot.show()
+    # =================================================================
+    # Step 3. Solve the fluid, the fluid-fluid interface, and the sheet deformation. 
 
     
