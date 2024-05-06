1f745f764ca2c6305d92fc6cb42ad1eba2272450
Zhixuan Li <zihinlai@163.com>
2024-05-06 12:48:41 +0800

    Implement the error calculation of the interfaces by shape difference.

--- analyzeMCL.py
+++ analyzeMCL.py
@@ -3,10 +3,15 @@ import numpy as np
 from matplotlib import pyplot
 from fem import *
 
-cp_group = "result/MCL-B0.05-s{:d}"
-cp_base_num = 4096
-base_dt = 1.0/1024/32
-num_hier = 2
+cp_group = (
+    "result/MCL-B0.1-g5.0-s0t0/0256.npz", 
+    "result/MCL-B0.1-g5.0-s1t2/1024.npz", 
+)
+base_dt = (
+    1.0/1024/16, 
+    1.0/1024/16/4,
+)
+num_hier = len(cp_group)
 
 @Functional
 def xdy_ydx(x: QuadData) -> np.ndarray:
@@ -31,29 +36,38 @@ def lift_to_P2(P2_space: FunctionSpace, p1_func: Function) -> Function:
         raise NotImplementedError
     return p2_func
 
-def interp_P1(fine_fs: FunctionSpace, coarse_fs: FunctionSpace, y: Function) -> Function:
-    # y: Function on the coarse space
-    rdim = fine_fs.elem.rdim
-    y_interp = Function(fine_fs)
-    fine_dof = fine_fs.elem_dof
-    coarse_dof = coarse_fs.elem_dof    
-    # According to the implementation of splitRefine, 
-    # the refined elements are ordered interlaced. 
-    for d in range(rdim):
-        y_interp[fine_dof[0*rdim+d, ::2]] = y[coarse_dof[0*rdim+d]]
-        y_interp[fine_dof[1*rdim+d, 1::2]] = y[coarse_dof[1*rdim+d]]
-        y_interp[fine_dof[1*rdim+d, ::2]] = 0.5 * (y[coarse_dof[0*rdim+d]] + y[coarse_dof[1*rdim+d]])
-    return y_interp
+def error_between_interface(y_coarse: Function, y_fine: Function) -> float:
+    elem_dof = y_fine.fe.elem_dof
+    segs = y_fine[elem_dof].view(np.ndarray) # (4, Ne)
+    segs = segs.T.reshape(-1, 2, 2) # (Ne, 2, 2)
+    segs_dir = segs[:,1] - segs[:,0]
+    segs_norm = np.linalg.norm(segs_dir, axis=1) # (Ne, )
+    segs_dir = segs_dir / segs_norm[:, np.newaxis]
+    y_c = y_coarse.reshape(-1, 2)
+
+    def point2polyline(p: np.ndarray) -> float:
+        a = segs - p[np.newaxis, np.newaxis] # (Ne, 2, 2)
+        a = np.linalg.norm(a.reshape(-1,2), axis=1) # (Ne*2, )
+        b = p[np.newaxis] - segs[:,0] # (Ne, 2)
+        b = np.sum(b * segs_dir, axis=1)
+        b = np.where(b < 0.0, np.inf, b)
+        b = np.where(b > segs_norm, np.inf, b)
+        b = segs[:,0] + b[:, np.newaxis] * segs_dir
+        b = np.linalg.norm(b - p[np.newaxis], axis=1) # (Ne, )
+        return np.min(np.concatenate((a, b)))
+    
+    e = [point2polyline(p.view(np.ndarray)) for p in y_c]
+    return max(e)
+
 
 if __name__ == "__main__":
 
-    table_header = [str(k+1) for k in range(num_hier-1)]
+    table_header = [str(k) for k in range(num_hier-1)]
     error_table = {
-        "y L^inf": [0.0] * (num_hier-1), 
-        "y L^2": [0.0] * (num_hier-1), 
+        "y": [0.0] * (num_hier-1), 
         "vol": [0.0] * (num_hier-1),
     }
-    print("\n * Showing t = {}".format(cp_base_num * base_dt))
+    # print("\n * Showing t = {}".format(cp_base_num * base_dt))
 
     for k in range(num_hier):
         # prepare the mesh
@@ -73,7 +87,7 @@ if __name__ == "__main__":
         Q_sp = FunctionSpace(s_mesh, VectorElement(LineP2, 2)) # for deformation and also for the fluid stress
 
         # read the data from checkpoint
-        cp_file = path.join(cp_group.format(k), "{:04d}.npz".format(cp_base_num * 2**k))
+        cp_file = path.join(cp_group[k])
         cp = np.load(cp_file)
         energy = cp["energy"] # type: np.ndarray
         refcl_hist = cp["refcl_hist"] # type: np.ndarray
@@ -81,18 +95,18 @@ if __name__ == "__main__":
 
         # plot the total energy
         if k == num_hier-1:
-            t_span = np.arange(energy.shape[0]) * base_dt / 2**k
+            t_span = np.arange(energy.shape[0]) * base_dt[k]
             # pyplot.figure()
             # pyplot.plot(t_span, np.sum(energy, axis=1), '-', label="total")
             # pyplot.legend()
             # pyplot.title(cp_file)
-            # plot the contact line motion
-            pyplot.figure()
-            pyplot.plot(t_span, refcl_hist[:,0], '-', label="ref left")
-            pyplot.plot(t_span, refcl_hist[:,2], '-', label="ref right")
-            pyplot.plot(t_span, phycl_hist[:,0], '-', label="phy left")
-            pyplot.plot(t_span, phycl_hist[:,2], '-', label="phy right")
-            pyplot.legend()
+            # # plot the contact line motion
+            # pyplot.figure()
+            # pyplot.plot(t_span, refcl_hist[:,0], '-', label="ref left")
+            # pyplot.plot(t_span, refcl_hist[:,2], '-', label="ref right")
+            # pyplot.plot(t_span, phycl_hist[:,0], '-', label="phy left")
+            # pyplot.plot(t_span, phycl_hist[:,2], '-', label="phy right")
+            # pyplot.legend()
 
         # extract the interface parametrization
         y_k = Function(Y_sp)
@@ -106,20 +120,21 @@ if __name__ == "__main__":
         id_k[:] = cp["id_k"]
         q_k += lift_to_P2(Q_sp, id_k)
         vol += xdy_ydx.assemble(Measure(s_mesh, dim=1, order=5, coord_map=q_k)) # type: float
+        print("volume at level {} = {}".format(k, vol))
 
         if k > 0:
-            # calculate the error of the interface
-            # Interpolate y_k_prev onto Y_sp:
-            y_k_interp = interp_P1(Y_sp, Y_sp_prev, y_k_prev)
-            y_k_err = y_k - y_k_interp # type: Function
-            ds = Measure(i_mesh, dim=1, order=3)
-            error_table["y L^2"][k-1] = np.sqrt(c_L2.assemble(ds, u=y_k_err._interpolate(ds)))
-            error_table["y L^inf"][k-1] = np.linalg.norm(y_k_err, ord=np.inf)
-            # calculate the error of the volume
+            error_table["y"][k-1] = error_between_interface(y_k_prev, y_k)
             error_table["vol"][k-1] = np.abs(vol - vol_prev)
+            # calculate the error of the cos contact angle
+            # m1 = cp["m1"]
+            # m3 = cp["m3"].reshape(2,2)
+            # error_table["ca L"][k-1] = np.abs(np.dot(m1[0], m3[0]) - cos_Young)
+            # error_table["ca R"][k-1] = np.abs(np.dot(m1[0], m3[0]) - cos_Young)
+        # keep the results for the next level
         y_k_prev = y_k # type: Function
         Y_sp_prev = Y_sp # type: FunctionSpace
         vol_prev = vol
 
-    printConvergenceTable(table_header, error_table)
+    if num_hier >= 2:
+        printConvergenceTable(table_header, error_table)
     pyplot.show()
