4647f3ea0917207cbfbdea65e0b2e56a4550deeb
Zhixuan Li <zihinlai@163.com>
2024-09-26 14:06:30 +0800

    Plate bending with jump looks OK on uniform and non-uniform grids; the jump needs to be tuned.

--- thin_film.py
+++ thin_film.py
@@ -37,34 +37,18 @@ def testFiniteDifference(xi_b: np.ndarray):
         ddk /= (xi_c[k:] - xi_c[:-k])[:,np.newaxis]
         dd_table.append(ddk)
 
-    # 3. test the Poisson equation
-    val_up = np.zeros((n+1, ))
-    val_up[1:] = -2 * dd_table[2][1:-1,2]
-    val_lo = np.zeros((n+1, ))
-    val_lo[:-1] = -2 * dd_table[2][1:-1,0]
-    val_diag = np.zeros((n+2, ))
-    val_diag[1:-1] = -2 * dd_table[2][1:-1,1]
-    # set up the Dirichlet condition
-    val_diag[0] = 1.0/2; val_up[0] = 1.0/2
-    val_diag[-1] = 1.0/2; val_lo[-1] = 1.0/2
+    # 3. build the negative Laplacian operator
+    val_up = np.zeros((n+3, ))
+    val_up[2:-1] = -2 * dd_table[2][1:-1,2]
+    val_lo = np.zeros((n+3, ))
+    val_lo[1:-2] = -2 * dd_table[2][1:-1,0]
+    val_diag = np.zeros((n+4, ))
+    val_diag[2:-2] = -2 * dd_table[2][1:-1,1]
     # assemble the matrix
-    A = sp.diags((val_diag, val_up, val_lo), (0, 1, -1), (n+2, n+2), "csr")
-    # prepare the rhs
-    b = np.zeros((n+2, ))
-    b[1:-1] = -ddu(xi_c[2:-2])
-    b[0] = u(xi_b[0]); b[-1] = u(xi_b[-1])
-    # solve the linear system
-    uu = spsolve(A, b)
-    # plot the solution
-    _, ax = pyplot.subplots()
-    ax.plot(xi_c[2:-2], uu[1:-1], 'o', label="numeric")
-    ax.plot(xi_c[2:-2], u(xi_c[2:-2]), '-', label="reference")
-    ax.legend(); ax.set_title("Poisson"); # pyplot.draw()
-    err = uu[1:-1] - u(xi_c[2:-2])
-    print("Poisson, n = {}, inf-norm error = {:.2e}".format(n, np.linalg.norm(err, ord=np.inf)))
-    del val_up, val_lo, val_diag, A, b
+    L = sp.diags((val_diag, val_up, val_lo), (0, 1, -1), (n+4, n+4), "csr")
+    del val_up, val_lo, val_diag
 
-    # 4. test the biharmonic equation
+    # 4. build the biharmonic operator
     val_up1 = np.zeros((n+3, ))
     val_up1[2:-1] = 24.0 * dd_table[4][:,3]
     val_up2 = np.zeros((n+2, ))
@@ -75,38 +59,52 @@ def testFiniteDifference(xi_b: np.ndarray):
     val_lo2[:-2] = 24.0 * dd_table[4][:,0]
     val_diag = np.zeros((n+4, ))
     val_diag[2:-2] = 24.0 * dd_table[4][:,2]
+    A = sp.diags((val_diag, val_up1, val_up2, val_lo1, val_lo2), (0, 1, 2, -1, -2), (n+4, n+4), "csr")
+
     # set the ghost extrapolation
+    val_up1[:] = 0.0; val_up2[:] = 0.0; val_diag[:] = 0.0; val_lo1[:] = 0.0; val_lo2[:] = 0.0
     val_up3 = np.zeros((n+1, ))
     val_diag[0] = -1/16; val_up1[0] = 9/16; val_up2[0] = 9/16; val_up3[0] = -1/16 # Dirichlet
-    val_lo1[0] = 1/(24*h[0]); val_diag[1] = -9/(8*h[0]); val_up1[1] = 9/(8*h[0]); val_up2[1] = -1/(24*h[0]) # Neumann
+    # val_lo1[0] = 1/(24*h[0]); val_diag[1] = -9/(8*h[0]); val_up1[1] = 9/(8*h[0]); val_up2[1] = -1/(24*h[0]) # Neumann
+    val_lo1[0] = 1.0; val_diag[1] = -1.0; val_up1[1] = -1.0; val_up2[1] = 1.0; # second-order
     val_lo3 = np.zeros((n+1, ))
     val_lo2[-2] = 1/(24*h[1]); val_lo1[-2] = -9/(8*h[1]); val_diag[-2] = 9/(8*h[1]); val_up1[-1] = -1/(24*h[1]) # Neumann
-    val_lo3[-1] = -1/16; val_lo2[-1] = 9/16; val_lo1[-1] = 9/16; val_diag[-1] = -1/16 # Dirichlet
-    # prepare the linear system
-    A = sp.diags((val_diag, val_up1, val_up2, val_up3, val_lo1, val_lo2, val_lo3), 
+    val_lo3[-1] = -1/h[1]**3; val_lo2[-1] = 3/h[1]**3; val_lo1[-1] = -3/h[1]**3; val_diag[-1] = 1/h[1]**3 # third-order derivative
+    G = sp.diags((val_diag, val_up1, val_up2, val_up3, val_lo1, val_lo2, val_lo3), 
                  (0, 1, 2, 3, -1, -2, -3), (n+4, n+4), "csr")
+    del val_up1, val_up2, val_up3, val_lo1, val_lo2, val_lo3, val_diag
+
+    # account for the jump
+    c = np.where(xi_c >= 0.5, 0.5 * 1.0/6*(xi_c - 0.5)**3, 0.0)
+    Ac = A @ c
+
+    # prepare the linear system
+    bm = 1e-5 # the bending modulus
     b = np.zeros((n+4, ))
-    b[2:-2] = d4u(xi_c[2:-2])
-    b[0] = u(xi_b[0]); b[1] = du(xi_b[0])
-    b[-2] = du(xi_b[-1]); b[-1] = u(xi_b[-1])
+    b[2:-2] = np.where(xi_c[2:-2] >= 0.5, -1.0, 0.0)
+    # b[0] = u(xi_b[0]); b[1] = du(xi_b[0])
+    # b[-2] = du(xi_b[-1]); b[-1] = u(xi_b[-1])
     # solve the linear system
-    uu = spsolve(A, b)
+    uu = spsolve(bm * A + L + G, b + Ac)
+    # Ar = (bm * A + L + G)[1:][:,1:]
+    # br = (b + Ac)[1:]
+    # uur = spsolve(Ar, br)
+    # uu = np.concatenate(((0.0,), uur))
     _, ax = pyplot.subplots()
-    ax.plot(xi_c[2:-2], uu[2:-2], 'o', label="numeric")
-    ax.plot(xi_c[2:-2], u(xi_c[2:-2]), '-', label="reference")
-    ax.legend(); ax.set_title("Biharmonic"); #pyplot.draw()
-    err = uu[2:-2] - u(xi_c[2:-2])
-    print("Biharmonic, n = {}, inf-norm error = {:.2e}".format(n, np.linalg.norm(err, ord=np.inf)))
-    del val_up1, val_up2, val_up3, val_lo1, val_lo2, val_lo3, val_diag, A, b
+    ax.plot(xi_c[2:-2], uu[2:-2], '-', label="numeric")
+    # ax.plot(xi_c[2:-2], u(xi_c[2:-2]), '-', label="reference")
+    ax.legend(); ax.axis("equal"); ax.set_title("Plate bending with tension"); #pyplot.draw()
+    # err = uu[2:-2] - u(xi_c[2:-2])
+    # print("Biharmonic, n = {}, inf-norm error = {:.2e}".format(n, np.linalg.norm(err, ord=np.inf)))
 
 
 
 if __name__ == "__main__":
-    # xi_b = np.linspace(0.0, 1.0, 33)
+    # xi_b = np.linspace(0.0, 1.0, 65)
     xi_b = np.concatenate((
-        np.linspace(0.0, 0.25, 33), 
-        np.linspace(0.25, 0.5, 17)[1:], 
-        np.linspace(0.5, 1.0, 9)[1:]
+        np.linspace(0.0, 3/8, 49), 
+        np.linspace(3/8, 5/8, 65)[1:], 
+        np.linspace(5/8, 1.0, 49)[1:]
     ))
     #
     testFiniteDifference(xi_b)
