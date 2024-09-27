f7522b6bc8d98d1a399695befa5846e2bf862521
Zhixuan Li <zihinlai@163.com>
2024-09-27 11:30:24 +0800

    Thin film spreading with constant CL velocity looks OK; a precursor film appears in fast spreading.

--- thin_film.py
+++ thin_film.py
@@ -18,7 +18,7 @@ def testFiniteDifference(xi_b: np.ndarray):
     n = xi_b.size - 1 # number of cells
     h = xi_b[1] - xi_b[0], xi_b[-1] - xi_b[-2]
     xi_g = np.concatenate(((xi_b[0] - 2*h[0], xi_b[0] - h[0]), xi_b, (xi_b[-1] + h[1], xi_b[-1] + 2*h[1]))) # add ghost points
-    xi_c = (xi_g[:-1] + xi_g[1:]) / 2 # get the cell centers
+    xi_c = (xi_g[:-1] + xi_g[1:]) / 2 # get the cell centers (with ghosts)
 
     # 2. build the divided difference table. 
     dd_table = [None]
@@ -38,7 +38,10 @@ def testFiniteDifference(xi_b: np.ndarray):
         dd_table.append(ddk)
 
     # initial value
-    slip = 1e-3 # the slip length
+    slip = 1e-4 # the slip length
+    a = 1.0 # the CL position
+    adot = 1e1
+    # adot = 0.0
     h = np.zeros((n+4, ))
     h[2:-2] = 1 - np.exp(5.0*(xi_c[2:-2]-1))
     h[2:-2] *= 0.4 + 0.05 * np.cos(20*xi_c[2:-2])
@@ -48,14 +51,17 @@ def testFiniteDifference(xi_b: np.ndarray):
     ax.plot(xi_c[2:-2], h[2:-2], '-')
     ax.set_ylim(-0.1, 0.5); ax.axis("equal")
 
-    dt = 1.0/1024/128
+    dt = 1.0/(1024*1024)
     Ca = 1.0
-    for m in range(256):
-        # fill the ghosts near the symmetric boundary at x=0
+    maxStep = 1024
+    for m in range(maxStep):
+        a = 1.0 + m * dt * adot
+        # fill the ghosts near the symmetric boundary at x=0 and at x=a
         h[0], h[1] = h[3], h[2]
+        h[-2] = -h[-3]
         # interpolate to cell boundaries
         h_mid = (h[3:-2] + h[2:-3]) / 2 # (n-1, )
-        # calculate the fluxes at the cell boundaries
+        # calculate the flux coefficients at the cell boundaries
         fc = np.concatenate(((0.0,), h_mid**2 * (h_mid/3 + slip), (0.0,))) # (n+1, ), zero flux at both boundaries
         # build the FD scheme
         fdtab = np.zeros((n, 5))
@@ -75,7 +81,7 @@ def testFiniteDifference(xi_b: np.ndarray):
         v_up2[2:] = fdtab[:-1,4]
         v_lo2 = np.zeros((n+1, ))
         v_lo2[:-1] = fdtab[:,0]
-        C = sp.diags((v_diag, v_up1, v_up2, v_lo1, v_lo2), (0, 1, 2, -1, -2), (n+3, n+3), "csr")
+        LL = sp.diags((v_diag, v_up1, v_up2, v_lo1, v_lo2), (0, 1, 2, -1, -2), (n+3, n+3), "csr")
         # assemble the boundary conditions
         v_diag[:] = 0.0; v_up1[:] = 0.0; v_up2[:] = 0.0; v_lo1[:] = 0.0; v_lo2[:] = 0.0
         v_up1[0] = -1.0; v_up2[0] = 1.0 # symmetry
@@ -85,18 +91,26 @@ def testFiniteDifference(xi_b: np.ndarray):
         v_diag[:] = 0.0; v_diag[2:-1] = 1.0
         I = sp.diags((v_diag, ), (0, ), (n+3, n+3), "csr")
         del v_diag, v_up1, v_up2, v_lo1, v_lo2
+        # calculate the advection term using upwind
+        adv = np.zeros((n, ))
+        adv[:-1] = (h[3:-2] - h[2:-3]) / (xi_c[3:-2] - xi_c[2:-3]) # (n-1, )
+        adv[-1] = -2*h[-3] / (xi_b[-1] - xi_b[-2])
+        adv *= xi_c[2:-2] * adot / a
         # assemble the linear system
-        A = I + (dt / Ca) * C + G
+        A = I + (dt / (Ca * a**4)) * LL + G
         b = np.zeros((n+3, ))
-        b[2:-1] = h[2:-2]
+        b[2:-1] = h[2:-2] + dt * adv
         h_next = spsolve(A, b)
+        diff = h_next[2:-1] - h[2:-2]
         h[:-1] = h_next
+        vol = np.sum(h_next[2:-1] * a * (xi_b[1:] - xi_b[:-1]))
+        assert np.all(h[2:-2] >= 0.)
         #
         ax.clear()
-        ax.plot(xi_c[2:-2], h[2:-2], '-')
-        ax.set_title("t = {}".format((m+1)*dt)); 
-        ax.set_ylim(-0.1, 0.5); # ax.axis("equal")
-        pyplot.draw(); pyplot.pause(0.1)
+        ax.plot(a * xi_c[2:-2], h[2:-2], '-')
+        print("t = {:.6f}, step = {}/{}, diff = {:.2e}, vol = {:.4e}, a = {:.5f}".format((m+1)*dt, m, maxStep, np.linalg.norm(diff, np.inf), vol, a)); 
+        ax.set_xlim(0.0, 1.5); ax.set_ylim(-0.1, 0.5); # ax.axis("equal")
+        pyplot.draw(); pyplot.pause(1e-2)
         pass
 
     pyplot.ioff()
@@ -162,12 +176,14 @@ def testFiniteDifference(xi_b: np.ndarray):
 
 
 if __name__ == "__main__":
-    xi_b = np.linspace(0.0, 1.0, 65)
-    # xi_b = np.concatenate((
-    #     np.linspace(0.0, 3/8, 49), 
-    #     np.linspace(3/8, 5/8, 65)[1:], 
-    #     np.linspace(5/8, 1.0, 49)[1:]
-    # ))
+    # xi_b = np.linspace(0.0, 1.0, 65)
+    xi_b = np.concatenate((
+        np.linspace(0.0, 0.5, 33), 
+        np.linspace(1/2, 3/4, 33)[1:], 
+        np.linspace(3/4, 7/8, 33)[1:],
+        np.linspace(7/8, 15/16, 33)[1:],
+        np.linspace(15/16, 1.0, 65)[1:],
+    ))
     #
     testFiniteDifference(xi_b)
     pyplot.show()
