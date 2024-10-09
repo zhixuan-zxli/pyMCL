859aca6410d4087a0d2a1339527447c08e2e82b9
Zhixuan Li <zihinlai@163.com>
2024-10-09 17:31:33 +0800

    Runner for liquid looks OK.

--- thin_film.py
+++ thin_film.py
@@ -4,9 +4,6 @@ from scikits.umfpack import spsolve
 from matplotlib import pyplot
 from runner import *
 
-# test Poisson equation
-# def testFiniteDifference(xi_b: np.ndarray):
-
 @dataclass
 class PhysicalParameters:
     gamma: tuple[float] = (1.0, 1.0, 5.0) # the (effective) surface tension for the wet, dry and the interface
@@ -42,11 +39,11 @@ class ThinFilmRunner(Runner):
         
         dxi = xi_b[1:] - xi_b[:-1] # mesh step size on the reference domain
         xi_g = np.concatenate(((xi_b[0] - 2*dxi[0], xi_b[0] - dxi[0]), xi_b, (xi_b[-1] + dxi[-1], xi_b[-1] + 2*dxi[-1]))) # add ghost points
-        xi_c = (xi_g[:-1] + xi_g[1:]) / 2 # get the cell centers (with ghosts)
-        xi_c_fluid = xi_c[:n_fluid+3]     # the fluid cells, with ghosts
+        xi_c = (xi_g[:-1] + xi_g[1:]) / 2 # get the cell centers (with 2 ghosts)
+        xi_c_f = xi_c[:n_fluid+3]         # the fluid cells, with one ghost on the right end
         self.min_dxi = np.min(dxi)
         self.dxi_at_cl = xi_c[n_fluid+2] - xi_c[n_fluid+1]
-        self.xi_c, self.xi_c_fluid = xi_c, xi_c_fluid
+        self.xi_c, self.xi_c_f = xi_c, xi_c_f
         
         # 2. build the divided difference table. 
         dd_table = [None]
@@ -67,8 +64,8 @@ class ThinFilmRunner(Runner):
         self.dd_table = dd_table
 
         # 3. set initial values
-        self.h = 1 - np.exp(4.0*(xi_c_fluid-1))
-        self.h *= 1.0 + 0.2 * np.cos(20*xi_c_fluid)
+        self.h = 1 - np.exp(4.0*(xi_c_f-1))
+        self.h *= 1.0 + 0.2 * np.cos(20*xi_c_f)
         self.h[-1] = -self.h[-2]
         self.g = np.zeros((n_total+3, ))
 
@@ -144,15 +141,11 @@ class ThinFilmRunner(Runner):
         xi_b_f = self.xi_b_f
         assert np.all(self.h[2:-1] >= self.g[2:2+n_fluid])
         vol = np.sum((self.h[2:-1] - self.g[2:2+n_fluid]) * self.a * (xi_b_f[1:] - xi_b_f[:-1]))
-        # delta_h = np.linalg.norm(h[2:-1] - self.h[2:-1], ord=np.inf) / solp.dt
-        # delta_g = np.linalg.norm(g[2:-1] - self.g[2:-1], ord=np.inf) / solp.dt
-        # print("t = {:.6f}, dt = {:.3e}, diff = {:.2e}, {:.2e}, vol = {:.4e}, a = {:.5f}, adot = {:.2e}".format(
-        #     t, solp.dt, delta_h, delta_g, vol, a, adot))
         print("t = {:.6f}, dt = {:.3e}, vol = {:.3e}, ".format(self.t, self.solp.dt, vol), end="")
-        
+        # save intermediate result
         #
         self.ax.clear()
-        self.ax.plot(self.a * self.xi_c_fluid[2:], self.h[2:], '-')
+        self.ax.plot(self.a * self.xi_c_f[2:], self.h[2:], '-')
         self.ax.plot(self.a * self.xi_c[2:-2], self.g[2:-1], '--')
         self.ax.set_xlim(0.0, 3.0); self.ax.set_ylim(-0.1, 2.9); # ax.axis("equal")
         pyplot.draw(); pyplot.pause(1e-4)
@@ -161,92 +154,92 @@ class ThinFilmRunner(Runner):
     
     def main_step(self) -> None:
            
-        # # numSteps = ceil(solp.Te / solp.dt)
-        # # for m in range(numSteps):
-        # while t < solp.Te:
-        #     # 1. assemble the fourth-order thin film operator for h
-        #     # interpolate to cell boundaries
-        #     h_mid = (h[2:-2] + h[3:-1]) / 2                  # (n_fluid-1, )
-        #     g_mid = (g[2:2+n_fluid-1] + g[3:2+n_fluid]) / 2  # (n_fluid-1, )
-        #     # calculate the flux coefficients at the cell boundaries
-        #     fc = h_mid*(h_mid**2-g_mid**2) / 2 - (h_mid**3 - g_mid**3) / 6  - (g_mid/2 + phyp.slip) * (h_mid - g_mid)**2
-        #     fc = np.concatenate(((0.0,), fc, (0.0, )))  # (n_fluid+1, ), zero flux at both boundaries
-        #     # build the FD scheme
-        #     ctab = np.zeros((n_fluid, 5))
-        #     ctab[:,0] = -fc[:-1] * dd_table[3][:n_fluid, 0]   # (n_fluid, )
-        #     ctab[:,4] = fc[1:] * dd_table[3][1:n_fluid+1, 3]  # (n_fluid, )
-        #     for j in range(1, 4):
-        #         ctab[:,j] = fc[1:] * dd_table[3][1:n_fluid+1,j-1] - fc[:-1] * dd_table[3][:n_fluid,j] # (n, )
-        #     ctab = 6 * ctab / (xi_b_f[1:] - xi_b_f[:-1])[:,np.newaxis]
-        #     # assemble the sparse matrix; dispose the rightmost ghost cell
-        #     v_diag = np.zeros((n_fluid+3, ))
-        #     v_diag[2:-1] = ctab[:,2]
-        #     v_up1 = np.zeros((n_fluid+2, ))
-        #     v_up1[2:] = ctab[:,3]
-        #     v_lo1 = np.zeros((n_fluid+2, ))
-        #     v_lo1[1:-1] = ctab[:,1]
-        #     v_up2 = np.zeros((n_fluid+1, ))
-        #     v_up2[2:] = ctab[:-1,4]
-        #     v_lo2 = np.zeros((n_fluid+1, ))
-        #     v_lo2[:-1] = ctab[:,0]
-        #     C = sp.diags((v_diag, v_up1, v_up2, v_lo1, v_lo2), (0, 1, 2, -1, -2), (n_fluid+3, n_fluid+3), "csr")
+        h, g = self.h, self.g
+        n_fluid = self.n_fluid
+        dd_table = self.dd_table
+        xi_b_f, xi_c_f = self.xi_b_f, self.xi_c_f
+        dxi_at_cl = self.dxi_at_cl
+        # 1. assemble the fourth-order thin film operator for h
+        # interpolate to cell boundaries
+        h_mid = (h[2:-2] + h[3:-1]) / 2                  # (n_fluid-1, )
+        g_mid = (g[2:2+n_fluid-1] + g[3:2+n_fluid]) / 2  # (n_fluid-1, )
+        # calculate the flux coefficients at the cell boundaries
+        fc = h_mid*(h_mid**2-g_mid**2) / 2 - (h_mid**3 - g_mid**3) / 6  - (g_mid/2 + self.phyp.slip) * (h_mid - g_mid)**2
+        fc = np.concatenate(((0.0,), fc, (0.0, )))  # (n_fluid+1, ), zero flux at both boundaries
+        # build the FD scheme
+        ctab = np.zeros((n_fluid, 5))
+        ctab[:,0] = -fc[:-1] * dd_table[3][:n_fluid, 0]   # (n_fluid, )
+        ctab[:,4] = fc[1:] * dd_table[3][1:n_fluid+1, 3]  # (n_fluid, )
+        for j in range(1, 4):
+            ctab[:,j] = fc[1:] * dd_table[3][1:n_fluid+1,j-1] - fc[:-1] * dd_table[3][:n_fluid,j] # (n, )
+        ctab = 6 * ctab / (xi_b_f[1:] - xi_b_f[:-1])[:,np.newaxis]
+        # assemble the sparse matrix; dispose the rightmost ghost cell
+        v_diag = np.zeros((n_fluid+3, ))
+        v_diag[2:-1] = ctab[:,2]
+        v_up1 = np.zeros((n_fluid+2, ))
+        v_up1[2:] = ctab[:,3]
+        v_lo1 = np.zeros((n_fluid+2, ))
+        v_lo1[1:-1] = ctab[:,1]
+        v_up2 = np.zeros((n_fluid+1, ))
+        v_up2[2:] = ctab[:-1,4]
+        v_lo2 = np.zeros((n_fluid+1, ))
+        v_lo2[:-1] = ctab[:,0]
+        C = sp.diags((v_diag, v_up1, v_up2, v_lo1, v_lo2), (0, 1, 2, -1, -2), (n_fluid+3, n_fluid+3), "csr")
 
-        #     # calculate the CL speed
-        #     tan_alpha = (h[-2] - h[-1]) / (a * dxi_at_cl)
-        #     assert tan_alpha >= 0.
-        #     tan_beta = (g[n_fluid+2] - g[n_fluid]) / (a * dxi_at_cl)
-        #     assert tan_beta >= 0.
-        #     theta_d = np.arctan(tan_alpha) + np.arctan(tan_beta)
-        #     adot = phyp.mu_cl * 0.5 * (theta_d**2 - phyp.theta_Y**2)
-        #     a_next = a + adot * solp.dt
+        # calculate the CL speed
+        tan_alpha = (h[-2] - h[-1]) / (self.a * dxi_at_cl)
+        assert tan_alpha >= 0.
+        tan_beta = (g[n_fluid+2] - g[n_fluid]) / (self.a * dxi_at_cl)
+        assert tan_beta >= 0.
+        theta_d = np.arctan(tan_alpha) + np.arctan(tan_beta)
+        adot = self.phyp.mu_cl * 0.5 * (theta_d**2 - self.phyp.theta_Y**2)
+        a_next = self.a + adot * solp.dt
 
-        #     # calculate the advection term using upwind
-        #     adv = np.zeros((n_fluid+3, ))
-        #     if adot >= 0.0:
-        #         adv[2:-1] = ((h[3:] - h[2:-1]) - (g[3:3+n_fluid] - g[2:2+n_fluid])) / (xi_c_fluid[3:] - xi_c_fluid[2:-1]) # (n_fluid, )
-        #     else:
-        #         adv[2:-1] = ((h[2:-1] - h[1:-2]) - (g[2:2+n_fluid] - g[1:1+n_fluid])) / (xi_c_fluid[2:-1] - xi_c_fluid[1:-2]) # (n_fluid, )
-        #     adv[2:-1] *= xi_c_fluid[2:-1] * adot / a_next
+        # calculate the advection term using upwind
+        adv = np.zeros((n_fluid+3, ))
+        if adot >= 0.0:
+            adv[2:-1] = ((h[3:] - h[2:-1]) - (g[3:3+n_fluid] - g[2:2+n_fluid])) / (xi_c_f[3:] - xi_c_f[2:-1]) # (n_fluid, )
+        else:
+            adv[2:-1] = ((h[2:-1] - h[1:-2]) - (g[2:2+n_fluid] - g[1:1+n_fluid])) / (xi_c_f[2:-1] - xi_c_f[1:-2]) # (n_fluid, )
+        adv[2:-1] *= xi_c_f[2:-1] * adot / a_next
 
-        #     # assemble the linear system
-        #     # A = sp.bmat((
-        #     #     (Ihh + (solp.dt*phyp.gamma[2]/a**4)*C + G4hh, -Ihg-G4hg), 
-        #     #     (phyp.gamma[2]*L4h/a**2, phyp.bm*LL/a**4+gammaL/a**2+G)
-        #     #     ), format="csr")
-        #     # # incorporate the jump
-        #     # dummy_jump = np.zeros((n_total+3, ))
-        #     # dummy_jump[2:] = np.minimum(-phyp.gamma[2] * theta_d / 6 * ((xi_c[2:-1] - 1.0) * a)**3, 0.0)
-        #     # b = np.concatenate((solp.dt * adv, LL @ dummy_jump / a**4))
-        #     # x = spsolve(A, b)
-        #     # h_next = x[:n_fluid+3]
-        #     # g_next = x[n_fluid+3:]
-        #     h_g = np.zeros_like(h)
-        #     h_g[2:-1] = h[2:-1] - g[2:n_fluid+2]
-        #     h_next = spsolve(Ihh + (solp.dt * phyp.gamma[2]/a_next**4)*C + G4hh, solp.dt * adv + h_g)
-        #     g_next = np.zeros_like(g)
-        #     a += adot * solp.dt
+        # assemble the linear system
+        # A = sp.bmat((
+        #     (Ihh + (solp.dt*phyp.gamma[2]/a**4)*C + G4hh, -Ihg-G4hg), 
+        #     (phyp.gamma[2]*L4h/a**2, phyp.bm*LL/a**4+gammaL/a**2+G)
+        #     ), format="csr")
+        # # incorporate the jump
+        # dummy_jump = np.zeros((n_total+3, ))
+        # dummy_jump[2:] = np.minimum(-phyp.gamma[2] * theta_d / 6 * ((xi_c[2:-1] - 1.0) * a)**3, 0.0)
+        # b = np.concatenate((solp.dt * adv, LL @ dummy_jump / a**4))
+        # x = spsolve(A, b)
+        # h_next = x[:n_fluid+3]
+        # g_next = x[n_fluid+3:]
+        h_g = np.zeros_like(h)
+        h_g[2:-1] = h[2:-1] - g[2:n_fluid+2]
+        h_next = spsolve(self.Ihh + (solp.dt * self.phyp.gamma[2]/a_next**4)*C + self.G4hh, solp.dt * adv + h_g)
+        g_next = np.zeros_like(g)
 
-
-        #     h[:] = h_next
-        #     g[:] = g_next
-        #     a = a_next
-
-
-        #     # adaptively change the dt
-        #     t += solp.dt
-        #     step += 1
-        #     if step % 32 == 0 and solp.dt < min_dxi / adot / 16 and solp.dt < min_dxi * 4:
-        #         solp.dt *= 2
-        self.t += self.solp.dt
         # some other info: 
-        # delta, a, adot
-        print(" ")
-        
+        delta_h = np.linalg.norm(h_next[2:-1] - h[2:-1], ord=np.inf) / solp.dt
+        delta_g = np.linalg.norm(g_next[2:-1] - g[2:-1], ord=np.inf) / solp.dt
+        print("diff = {:.2e}, {:.2e}, a_next = {:.5f}, adot = {:.2e}".format(
+            delta_h, delta_g, a_next, adot))
+
+        self.h[:] = h_next
+        self.g[:] = g_next
+        self.a = a_next
+
+        # adaptively change the dt
+        self.t += self.solp.dt
+        # self.step += 1 # done in Runner.run()
+        if self.step % 32 == 0 and solp.dt < self.min_dxi / adot / 16 and solp.dt < self.min_dxi * 4:
+            solp.dt *= 2
 
 
 if __name__ == "__main__":
 
-    solp = SolverParameters(dt = 1/(1024*4*4), Te=1.0)
+    solp = SolverParameters(dt = 1/(1024*4*4), Te=16.0)
  
     ThinFilmRunner(solp).run()
 
