71ffee5ddcec631b1b1dbc3f987b96f60ae96341
Zhixuan Li <zihinlai@163.com>
2025-01-26 16:07:18 +0800

    Update the plotting code.

--- analyze_thin_film.py
+++ analyze_thin_film.py
@@ -92,8 +92,8 @@ def plotCLSpeed() -> None:
     ax2.set_xlabel("$a$"); ax2.set_ylabel("$\\dot{a}(t)$"); ax2.legend()
 
 def plotProfiles() -> None:
-    filename = "tf-s-4-g2-Y1-rec"
-    cp_list = [1, 8, 24, 48]
+    filename = "tf-s-4-g2-Y1-adv"
+    cp_list = [2, 8]
     with open("result/" + filename + "/PhysicalParameters", "rb") as f:
         phyp = pickle.load(f)
     phyp.eps = -1.0 / np.log(phyp.slip)
@@ -115,19 +115,19 @@ def plotProfiles() -> None:
         g = data["g"]
         t = a_hist[0, -1]
         a = a_hist[1, -1]
+        n_fluid = h.size - 3
         adot = (a_hist[1,1:] - a_hist[1,:-1]) / (a_hist[0,1:] - a_hist[0,:-1])
         print("Plotting checkpoint {}, adot = {:.3e}".format(cp, adot[-1]))
         label = "$t={:.1f}$".format(t)
         # plot the profiles
-        n_fluid = h.size - 3
-        ax1.plot(a * xi_c[2:-2], g[1:-1], "-", color="k", label=label, alpha=alpha)
-        ax1.plot(a * xi_c[2:2+n_fluid], h[2:-1], "-", color="tab:blue", alpha=alpha)
+        ax1.plot(a * xi_c[2:-2:4], g[1:-1:4], "o", mfc='none', mec="tab:orange", label=label, alpha=alpha)
+        ax1.plot(a * xi_c[2:2+n_fluid:4], h[2:-1:4], "o", mfc='none', mec="tab:blue", alpha=alpha)
         # calculate and plot the slopes
         z = np.log(a * (1.0 - xi_c[2:n_fluid+2])) * phyp.eps + 1.0
         dh = (h[3:n_fluid+3] - h[1:n_fluid+1]) / (xi_c[3:n_fluid+3] - xi_c[1:n_fluid+1]) / a
         dg = (g[2:n_fluid+2] - g[:n_fluid]) / (xi_c[3:n_fluid+3] - xi_c[1:n_fluid+1]) / a
-        ax2.plot(z, -dh, 'o', markerfacecolor='none', label=label, alpha=alpha)
-        ax3.plot(z, -dg, 'o', markerfacecolor='none', label=label, alpha=alpha)
+        ax2.plot(z[::2], -dh[::2], 'o', mfc='none', mec="tab:blue", label=label, alpha=alpha)
+        ax3.plot(z[::2], -dg[::2], 'o', mfc='none', mec="tab:orange", label=label, alpha=alpha)
         # plot the theoretical prediction
         plotPrediction(xi_c[2:2+n_fluid], xi_c[2:-2], phyp, a, adot[-1], alpha, ax1, ax2, ax3)
     ax1.legend(); ax1.set_xlabel("$x$"); ax1.set_xlim(0.0, 2.5); ax1.set_ylabel("$z$")
@@ -158,65 +158,76 @@ def plotPrediction(xi_f: np.ndarray, xi_s: np.ndarray, phyp: PhysicalParameters,
         a_app = 3*phyp.vol / (a**2 * (1+1/phyp.gamma[0]))
         b_app = -1/phyp.gamma[0] * a_app
         th_app = 3*phyp.vol / a**2
-        B0 = phyp.bm / eps**2 #np.sqrt(phyp.bm / phyp.gamma[0]) / eps
+        Cb = phyp.bm / eps**2 #np.sqrt(phyp.bm / phyp.gamma[0]) / eps
         b_til = np.sqrt(phyp.gamma[0]) / (np.sqrt(phyp.gamma[0]) + np.sqrt(phyp.gamma[1])) * b_app
         a0 = ((a_app - b_til)**3 - th_y**3) / 3
         ea1 = adot / eps - a0
         # ================== Outer region ==================
         h0 = a_app * a * (1.0 - xi_f**2) / 2
         xf = a * xi_f
-        # h1 = eps * a0 / th_app**2 * ((a+xf) * np.log(a+xf) + (a-xf)*np.log(a-xf) - 2*a*np.log(2*a) + 3*a/2*(1.0-xi_f**2)) # the first order correction
+        h1 = a0 / th_app**2 * ((a+xf) * np.log(a+xf) + (a-xf)*np.log(a-xf) - 2*a*np.log(2*a) + 3*a/2*(1.0-xi_f**2)) # the first order correction
         xs = a * xi_s
         g0 = np.where(xi_s <= 1.0, b_app * a * (1.0 - xi_s**2) / 2, 0.0)
-        # g1 = np.where(xi_s <= 1.0, \
-        #     -eps * a0 / phyp.gamma[0] / th_app**2 * ((a+xs) * np.log(a+xs) + (a-xs)*np.log(a-xs) - 2*a*np.log(2*a) + 3*a/2*(1.0-xi_s**2)), \
-        #     0.0)
-        # ax1.plot(xf, h0 + h1, '--', color='k', alpha=alpha)
-        # ax1.plot(xs, g0 + g1, '--', color='k', alpha=alpha)
+        g1 = np.where(xi_s <= 1.0, \
+            -a0 / phyp.gamma[0] / th_app**2 * ((a+xs) * np.log(a+xs) + (a-xs)*np.log(a-xs) - 2*a*np.log(2*a) + 3*a/2*(1.0-xi_s**2)), \
+            0.0)
+        # ax1.plot(xf, h0 + eps * h1, 'k-', label="asymptotics")
+        # ax1.plot(xs, g0 + eps * g1, 'k-')
         y = a - xf
         z = np.log(y) * eps + 1.0
-        ax2.plot(z, a_app * xf / a + eps * a0 / th_app**2 * (np.log(y) + (3-np.log(2*a))), 'c-', alpha=alpha)
-        # ax3.plot(z, b_app * xf / a - eps * a0 / phyp.gamma[0] / th_app**2 * (np.log(y) + (3-np.log(2*a))), 'c:', alpha=alpha)
+        dh_outer = a_app * xf / a + eps * a0 / th_app**2 * (np.log(y) + (3-np.log(2*a)))
+        mask = z > 0.7
+        ax2.plot(z[mask], dh_outer[mask], 'k-', label="Outer")
 
         # ================== Inner region ==================
         s = y / phyp.slip
-        ax2.plot(z, th_y + b_til + eps * a0 / th_y * (np.log(th_y*s + 1) / th_y + s * np.log(1 + 1/(th_y*s)) + 1), 'g-', alpha=alpha)
+        dh_inner = th_y + b_til + eps * a0 / th_y * (np.log(th_y*s + 1) / th_y + s * np.log(1 + 1/(th_y*s)) + 1)
+        mask = z < 0.2
+        ax2.plot(z[mask], dh_inner[mask], 'm--', label="Inner")
 
         # ================== Intermediate region ==================
         m0 = (th_y**3 + 3*a0*z)**(1/3)
         m01 = m0 + 1 / m0**2 * (ea1*z + eps * a0 * (th_y + np.log(th_y) + 1))
-        flag = z > 0.0
-        ax2.plot(z[flag], m01[flag] + b_til, 'm-', alpha=alpha)
+        mask = (z > 0.0) & (z < 0.5)
+        ax2.plot(z[mask], m01[mask] + b_til, 'm-', label="Intermediate")
         
         # ================== Bending region ==================
         gamma = phyp.gamma
         # calculate the constants in the leading-order solution
-        lb = [np.sqrt(B0 / gam) for gam in gamma] 
-        C_neg = lb[1] * np.sqrt(gamma[0]) / (np.sqrt(gamma[0]) + np.sqrt(gamma[1])) * b_app
-        C_pos = lb[0] * np.sqrt(gamma[1]) / (np.sqrt(gamma[0]) + np.sqrt(gamma[1])) * b_app
-        D = C_neg - C_pos
+        lb = [np.sqrt(Cb / gam) for gam in gamma] 
+        C2 = lb[1] * np.sqrt(gamma[0]) / (np.sqrt(gamma[0]) + np.sqrt(gamma[1])) * b_app
+        C1 = lb[0] * np.sqrt(gamma[1]) / (np.sqrt(gamma[0]) + np.sqrt(gamma[1])) * b_app
+        A = C2 - C1
         y_til = (a - xs) / eps
         g_til_0 = np.where(y_til >= 0.0, 
-                        D + b_app * y_til + C_pos * np.exp(-y_til / lb[0]), 
-                        C_neg * np.exp(y_til / lb[1]))
+                        A + b_app * y_til + C1 * np.exp(-y_til / lb[0]), 
+                        C2 * np.exp(y_til / lb[1]))
         # calculate the constants in the next-order solution
-        C1_til, C0_til = -a_app / a, 3 + np.log(eps) - np.log(2*a)
-        A = np.array(((gamma[0], -gamma[1]), (1.0/lb[0], 1.0/lb[1])))
-        b = np.array((B0/gamma[0]*C1_til, eps*a0 / (gamma[0] * th_app**2) * (0.57721 - np.log(lb[0]) - C0_til)))
-        D = dense_solve(A, b)
-        E1 = eps*a0 / th_app**2 * (-np.log(lb[0]) - C0_til)
-        F1 = gamma[0] / lb[0] * (D[1] - D[0])
-        z1 = np.maximum(0.0, y_til / lb[0])
+        E1 = np.log(2*a/(eps*lb[0])) - 3
+        A = np.array(((1.0, -1.0), (lb[0], lb[1])))
+        rhs = np.array((b_app / a, a0/(gamma[0]*th_app**2)*(0.57722 + E1)))
+        D = dense_solve(A, rhs)
+        F1 = Cb * (D[1]/gamma[1] - D[0]/gamma[0])
+        y1 = np.maximum(0.0, y_til / lb[0])
         g_til_1 = np.where(y_til >= 0.0, 
-                        D[0]*np.exp(-z1) + lb[0] / gamma[0] * (-C1_til*lb[0]/2*z1**2 + E1*z1 + F1 + eps*a0/th_app**2*(phi(z1) - z1*(np.log(z1)-1))), 
-                        D[1]*np.exp(y_til / lb[1]))
-        # ax1.plot(xs, (g_til_0 + eps * g_til_1) * eps, 'k:', alpha=alpha)
-        z1 = z1[z1 > 0.0]
-        dg_til_0 = b_app - C_pos / lb[0] * np.exp(-z1)
-        dg_til_1 = (-D[0]*np.exp(-z1) + lb[0]/gamma[0]*(eps*a0/th_app**2*(dphi(z1) - np.log(z1)) - C1_til*lb[0]*z1 + E1)) / lb[0]
-        dh_til_1 = C1_til * (z1*lb[0]) + a0/th_app**2*(np.log(z1*lb[0]) + (3.0+np.log(eps)-np.log(2*a)))
-        ax3.plot(np.log(z1 * lb[0] * eps) * eps + 1.0, dg_til_0 + eps * dg_til_1, 'r-', alpha=alpha)
-        ax2.plot(np.log(z1 * lb[0] * eps) * eps + 1.0, a_app + eps * (dh_til_1), 'r-', alpha=alpha) # y_til -> +infty    
+                        D[0]*lb[0]**2*np.exp(-y1) - b_app/(2*a)*y_til**2 + a0*lb[0]/(gamma[0]*th_app**2)*(phi(y1) - y1*np.log(y1) + (E1+1)*y1) + F1, 
+                        D[1]*lb[1]**2*np.exp(y_til / lb[1]))
+        #
+        h_til_0 = a_app * y_til + C2
+        h_til_1 = a0/th_app**2 * (y_til * np.log(y_til) + (2 + np.log(eps/(2*a)))*y_til) - a_app/(2*a)*y_til**2 + D[1]*lb[1]
+        ax1.plot(xs, (h_til_0 + eps * h_til_1) * eps, 'k-', label="asymptotics")
+        ax1.plot(xs, (g_til_0 + eps * g_til_1) * eps, 'k-')
+        # plot the slopes
+        y_til = y_til[y_til > 0.0]
+        y1 = y1[y1 > 0.0]
+        z = np.log(y_til * eps) * eps + 1.0
+        mask = (z > 0.3) & (z < 0.7)
+        dg_til_0 = b_app - C1 / lb[0] * np.exp(-y1)
+        dg_til_1 = -D[0]*lb[0]*np.exp(-y1) - b_app/a*y_til + a0/(gamma[0]*th_app**2)*(dphi(y1) - np.log(y1) + E1)
+        ax3.plot(z, dg_til_0 + eps * dg_til_1, 'k-', label="asymptotics")
+        dh_til_0 = a_app
+        dh_til_1 = a0/th_app**2*(np.log(y_til) + 3 + np.log(eps/(2*a))) - a_app/a*y_til
+        ax2.plot(z[mask], (dh_til_0 + eps*dh_til_1)[mask], 'k--', label="Bending") # y_til -> +infty    
 
 if __name__ == "__main__":
 
